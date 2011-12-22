0751968e9fd6efc7fa1c6c246e3295ffcc704f1d
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2011-12-22 11:44:06 +0000

    Fixed important packaging bug preventing sound applet to work in DEB package

--- js/ui/status/volume.js
+++ js/ui/status/volume.js
@@ -104,7 +104,7 @@ const MediaServer2PlayerIFace = {
 };
 
 /* global values */
-let icon_path = "/usr/share/cinnamon/theme/mediaplayer/";
+let icon_path = "/usr/share/cinnamon/theme/";
 let compatible_players = [ "clementine", "mpd", "banshee", "rhythmbox", "rhythmbox3", "pragha", "quodlibet", "guayadeque", "amarok", "googlemusicframe", "xbmc" ];
 let support_seek = [ "clementine", "banshee", "rhythmbox", "rhythmbox3", "quodlibet", "amarok" ];
 /* dummy vars for translation */
