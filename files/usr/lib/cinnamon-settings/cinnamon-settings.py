7cb2cb34fae3fa6f1a26b50e614781a3cb3c7e38
Gwendal Le Bihan <gwendal.lebihan.dev@gmail.com>
2012-01-29 15:18:31 +0100

    Added options to select the title bar buttons order and position

--- files/usr/lib/cinnamon-settings/cinnamon-settings.py
+++ files/usr/lib/cinnamon-settings/cinnamon-settings.py
@@ -879,6 +879,99 @@ class ChangeTimeWidget(Gtk.HBox):
             
             thread.start_new_thread(self._do_change_system_time, ())
 
+class TitleBarButtonsOrderSelector(Gtk.Table):
+    def __init__(self):
+        self.key = "/desktop/cinnamon/windows/button_layout"
+        super(TitleBarButtonsOrderSelector, self).__init__()
+        self.settings = gconf.client_get_default()
+        self.value = self.settings.get_string(self.key)
+        try:
+            left_items, right_items = self.value.split(":")
+        except:
+            left_items = right_items = ""
+        if len(left_items) > 0:
+            left_items = left_items.split(",")
+        else:
+            left_items = []
+        if len(right_items) > 0:
+            right_items = right_items.split(",")
+        else:
+            right_items = []
+        
+        label = Gtk.Label(_("Left side title bar buttons"))
+        label.set_alignment(0, 0.5)
+        self.attach(label, 0, 1, 0, 1, xoptions = Gtk.AttachOptions.FILL, yoptions=0, xpadding=2)
+        left_side_box = Gtk.HBox()
+        self.attach(left_side_box, 1, 2, 0, 1, yoptions=0, xpadding=2)
+        
+        label = Gtk.Label(_("Right side title bar buttons"))
+        label.set_alignment(0, 0.5)
+        self.attach(label, 0, 1, 1, 2, xoptions = Gtk.AttachOptions.FILL, yoptions=0, xpadding=2)
+        right_side_box = Gtk.HBox()
+        self.attach(right_side_box, 1, 2, 1, 2, yoptions=0, xpadding=2)
+        
+        self.left_side_widgets = []
+        self.right_side_widgets = []
+        for i in range(3):
+            self.left_side_widgets.append(Gtk.ComboBox())
+            self.right_side_widgets.append(Gtk.ComboBox())
+        
+        buttons = [
+            ("", ""),
+            ("close", _("Close")),
+            ("minimize", _("Minimize")),
+            ("maximize", _("Maximize"))
+        ]
+        
+        for i in self.left_side_widgets + self.right_side_widgets:
+            if i in self.left_side_widgets:
+                ref_list = left_items
+                index = self.left_side_widgets.index(i)
+            else:
+                ref_list = right_items
+                index = self.right_side_widgets.index(i)
+            model = Gtk.ListStore(str, str)
+            selected_iter = None
+            for button in buttons:
+                iter = model.insert_before(None, None)
+                model.set_value(iter, 0, button[0])                
+                model.set_value(iter, 1, button[1])
+                if index < len(ref_list) and ref_list[index] == button[0]:
+                    selected_iter = iter
+            i.set_model(model)
+            renderer_text = Gtk.CellRendererText()
+            i.pack_start(renderer_text, True)
+            i.add_attribute(renderer_text, "text", 1)
+            i.connect("changed", self.on_my_value_changed)
+            if selected_iter is not None:
+                i.set_active_iter(selected_iter)
+        
+        for i in self.left_side_widgets:
+            left_side_box.pack_start(i, False, False, 2)
+        for i in self.right_side_widgets:
+            right_side_box.pack_start(i, False, False, 2)
+    
+    def on_my_value_changed(self, widget):
+        active_iter = widget.get_active_iter()
+        if active_iter:
+            new_value = widget.get_model()[active_iter][0]
+        else:
+            new_value = None
+        left_items = []
+        right_items = []
+        for i in self.left_side_widgets + self.right_side_widgets:
+            active_iter = i.get_active_iter()
+            if active_iter:
+                value = i.get_model()[i.get_active_iter()][0]
+                if i != widget and value == new_value:
+                    i.set_active_iter(None)
+                elif value != "":
+                    if i in self.left_side_widgets:
+                        left_items.append(value)
+                    else:
+                        right_items.append(value)
+        self.settings.set_string(self.key, ','.join(str(item) for item in left_items) + ':' + ','.join(str(item) for item in right_items))
+        
 class MainWindow:
   
     # Change pages
@@ -1082,6 +1175,10 @@ class MainWindow:
         sidePage.add_widget(GConfComboBox(_("Window focus mode"),
                                             "/apps/metacity/general/focus_mode",
                                             [(i, i.title()) for i in ("click","sloppy","mouse")]))
+        sidePage.add_widget(TitleBarButtonsOrderSelector())
+        label = Gtk.Label()
+        label.set_markup("<i><small>%s</small></i>" % _("Note: If you change the title bar buttons order you will need to restart Cinnamon."))
+        sidePage.add_widget(label)
         
         sidePage = SidePage(_("Fonts"), "fonts.svg", self.content_box)
         self.sidePages.append((sidePage, "fonts"))
