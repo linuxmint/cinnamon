dbea33315827821798655e43b8c3bd43690311fc
Albert Pool <albertpool@solcon.nl>
2012-02-05 17:38:25 +0100

    Checking workspaces correctly - the previous one did not really work
    I hope the stability issues are fixed now.

--- js/ui/windowList.js
+++ js/ui/windowList.js
@@ -121,13 +121,13 @@ AppMenuButtonRightClickMenu.prototype = {
         let currentIndex = this.metaWindow.get_workspace().index();
         if (currentIndex > 0)
             this.metaWindow.change_workspace_by_index(currentIndex - 1, 1, global.get_current_time());
-        this._checkWorkspaces()
+        Main._checkWorkspaces()
     },
 
     _onMoveToRightWorkspace: function(actor, event){
         let currentIndex = this.metaWindow.get_workspace().index();
         this.metaWindow.change_workspace_by_index(currentIndex + 1, 1, global.get_current_time());
-        this._checkWorkspaces()
+        Main._checkWorkspaces()
     },
 
     _onSourceKeyPress: function(actor, event) {
