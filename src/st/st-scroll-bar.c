8dfc84af118f92bbe1999ad36b9c0e16c478b83e
Jasper St. Pierre <jstpierre@mecheye.net>
2012-02-14 10:48:00 -0500

    st-scroll-bar: Use clutter_actor_destroy in dispose
    https://bugzilla.gnome.org/show_bug.cgi?id=670034

--- src/st/st-scroll-bar.c
+++ src/st/st-scroll-bar.c
@@ -183,28 +183,25 @@ st_scroll_bar_dispose (GObject *gobject)
 
   if (priv->handle)
     {
-      g_signal_handlers_disconnect_by_func (priv->handle,
-                                            G_CALLBACK (handle_button_press_event_cb),
-                                            bar);
-      clutter_actor_unparent (priv->handle);
+      clutter_actor_destroy (priv->handle);
       priv->handle = NULL;
     }
 
   if (priv->bw_stepper)
     {
-      clutter_actor_unparent (priv->bw_stepper);
+      clutter_actor_destroy (priv->bw_stepper);
       priv->bw_stepper = NULL;
     }
 
   if (priv->fw_stepper)
     {
-      clutter_actor_unparent (priv->fw_stepper);
+      clutter_actor_destroy (priv->fw_stepper);
       priv->fw_stepper = NULL;
     }
 
   if (priv->trough)
     {
-      clutter_actor_unparent (priv->trough);
+      clutter_actor_destroy (priv->trough);
       priv->trough = NULL;
     }
 
