e310b6b31dcd45b4b514a97340af45191b9a044f
Jasper St. Pierre <jstpierre@mecheye.net>
2012-02-29 22:38:35 -0500

    Don't depend on removed MetaPlugin APIs
    Several MetaPlugin APIs that were just wrappers around other functions
    were removed - switch to using the other functions directly.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=671103

--- src/cinnamon-global.c
+++ src/cinnamon-global.c
@@ -577,14 +577,18 @@ void
 cinnamon_global_set_stage_input_mode (CinnamonGlobal         *global,
                                    CinnamonStageInputMode  mode)
 {
+  MetaScreen *screen;
+
   g_return_if_fail (CINNAMON_IS_GLOBAL (global));
 
+  screen = meta_plugin_get_screen (global->plugin);
+
   if (mode == CINNAMON_STAGE_INPUT_MODE_NONREACTIVE || global->gtk_grab_active)
-    meta_plugin_set_stage_reactive (global->plugin, FALSE);
+    meta_empty_stage_input_region (screen);
   else if (mode == CINNAMON_STAGE_INPUT_MODE_FULLSCREEN || !global->input_region)
-    meta_plugin_set_stage_reactive (global->plugin, TRUE);
+    meta_set_stage_input_region (screen, None);
   else
-    meta_plugin_set_stage_input_region (global->plugin, global->input_region);
+    meta_set_stage_input_region (screen, global->input_region);
 
   if (mode == CINNAMON_STAGE_INPUT_MODE_FOCUSED)
     cinnamon_global_focus_stage (global);
@@ -1065,7 +1069,7 @@ _cinnamon_global_set_plugin (CinnamonGlobal *global,
   global->gdk_screen = gdk_display_get_screen (global->gdk_display,
                                                meta_screen_get_screen_number (global->meta_screen));
 
-  global->stage = CLUTTER_STAGE (meta_plugin_get_stage (plugin));
+  global->stage = CLUTTER_STAGE (meta_get_stage_for_screen (global->meta_screen));
   global->stage_xwindow = clutter_x11_get_stage_window (global->stage);
   global->stage_gdk_window = gdk_x11_window_foreign_new_for_display (global->gdk_display,
                                                                      global->stage_xwindow);
