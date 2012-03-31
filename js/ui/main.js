GNOME/gnome-shell

Adapt to mutter moving to GSettings
https://bugzilla.gnome.org/show_bug.cgi?id=663429

js/ui/main.js
@@ -4,7 +4,6 @@ const Clutter = imports.gi.Clutter;
 const Gdk = imports.gi.Gdk;
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
-const GConf = imports.gi.GConf;
 const Lang = imports.lang;
 const Mainloop = imports.mainloop;
 const Meta = imports.gi.Meta;
@@ -135,13 +134,13 @@ function _initUserSession() {
 
     let shellwm = global.window_manager;
 
-    shellwm.takeover_keybinding('panel_run_dialog');
-    shellwm.connect('keybinding::panel_run_dialog', function () {
+    shellwm.takeover_keybinding('panel-run-dialog');
+    shellwm.connect('keybinding::panel-run-dialog', function () {
        getRunDialog().open();
     });
 
-    shellwm.takeover_keybinding('panel_main_menu');
-    shellwm.connect('keybinding::panel_main_menu', function () {
+    shellwm.takeover_keybinding('panel-main-menu');
+    shellwm.connect('keybinding::panel-main-menu', function () {
         overview.toggle();
     });
 
@@ -576,16 +575,6 @@ function _globalKeyPressHandler(actor, event) {
 
     // This relies on the fact that Clutter.ModifierType is the same as Gdk.ModifierType
     let action = global.display.get_keybinding_action(keyCode, modifierState);
-
-    // The screenshot action should always be available (even if a
-    // modal dialog is present)
-    if (action == Meta.KeyBindingAction.COMMAND_SCREENSHOT) {
-        let gconf = GConf.Client.get_default();
-        let command = gconf.get_string('/apps/metacity/keybinding_commands/command_screenshot');
-        if (command != null && command != '')
-            Util.spawnCommandLine(command);
-        return true;
-    }
 
     // Other bindings are only available to the user session when the overview is up and
     // no modal dialog is present.

linuxmint/cinnamon

js/ui/main.js
@@ -4,7 +4,6 @@ const Clutter = imports.gi.Clutter;
 const DBus = imports.dbus;
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
-const GConf = imports.gi.GConf;
 const Mainloop = imports.mainloop;
 const Meta = imports.gi.Meta;
 const Cinnamon = imports.gi.Cinnamon;
@@ -147,13 +146,13 @@ function _initUserSession() {
 
     let cinnamonwm = global.window_manager;
 
-    cinnamonwm.takeover_keybinding('panel_run_dialog');
-    cinnamonwm.connect('keybinding::panel_run_dialog', function () {
+    cinnamonwm.takeover_keybinding('panel-run-dialog');
+    cinnamonwm.connect('keybinding::panel-run-dialog', function () {
        getRunDialog().open();
     });
 
-    cinnamonwm.takeover_keybinding('panel_main_menu');
-    cinnamonwm.connect('keybinding::panel_main_menu', function () {
+    cinnamonwm.takeover_keybinding('panel-main-menu');
+    cinnamonwm.connect('keybinding::panel-main-menu', function () {
         expo.toggle();
     });
     
@@ -659,16 +658,6 @@ function _globalKeyPressHandler(actor, event) {
 
     // This relies on the fact that Clutter.ModifierType is the same as Gdk.ModifierType
     let action = global.display.get_keybinding_action(keyCode, modifierState);
-
-    // The screenshot action should always be available (even if a
-    // modal dialog is present)
-    if (action == Meta.KeyBindingAction.COMMAND_SCREENSHOT) {
-        let gconf = GConf.Client.get_default();
-        let command = gconf.get_string('/apps/metacity/keybinding_commands/command_screenshot');
-        if (command != null && command != '')
-            Util.spawnCommandLine(command);
-        return true;
-    }
 
     // Other bindings are only available to the user session when the overview is up and
     // no modal dialog is present.