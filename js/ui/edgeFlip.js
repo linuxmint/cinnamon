44172d425df48e3186fa655fd80d89fe4bead6d0
dalcde <dalcde@yahoo.com.hk>
2012-06-23 15:26:13 +0800

    Code Cleanup

--- js/ui/edgeFlip.js
+++ js/ui/edgeFlip.js
@@ -1,3 +1,4 @@
+// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
 const Main = imports.ui.main;
 const Clutter = imports.gi.Clutter;
 const St = imports.gi.St;
@@ -11,61 +12,61 @@ function EdgeFlipper(side, func){
 
 EdgeFlipper.prototype = {
     _init: function(side, func){
-	this.side = side;
-	this.func = func;
+        this.side = side;
+        this.func = func;
 
         this.enabled = true;
-	this.entered = false;
-	this.activated = false;
+        this.entered = false;
+        this.activated = false;
 
-	this._checkOver();
+        this._checkOver();
     },
 
     _checkOver: function(){
-	let mask;
-	[this.xMouse, this.yMouse, mask] = global.get_pointer();
-	if (this.side == St.Side.RIGHT){
-	    if (this.xMouse + 5 > global.screen_width){
-		this._onMouseEnter();
-	    } else {
-		this._onMouseLeave();
-	    }
-	} else if (this.side == St.Side.LEFT){
-	    if (this.xMouse < 5 ){
-		this._onMouseEnter();
-	    } else {
-		this._onMouseLeave();
-	    }
-	} else if (this.side == St.Side.BOTTOM){
-	    if (this.yMouse + 5 > global.screen_height) {
-		this._onMouseEnter();
-	    } else {
-		this._onMouseLeave();
-	    }
-	} else if (this.side == St.Side.TOP){
-	    if (this.yMouse < 5){
-		this._onMouseEnter();
-	    } else {
-		this._onMouseLeave();
-	    }
-	}
-	Mainloop.timeout_add(500, Lang.bind(this, this._checkOver));
+        let mask;
+        [this.xMouse, this.yMouse, mask] = global.get_pointer();
+        if (this.side == St.Side.RIGHT){
+            if (this.xMouse + 5 > global.screen_width){
+                this._onMouseEnter();
+            } else {
+                this._onMouseLeave();
+            }
+        } else if (this.side == St.Side.LEFT){
+            if (this.xMouse < 5 ){
+                this._onMouseEnter();
+            } else {
+                this._onMouseLeave();
+            }
+        } else if (this.side == St.Side.BOTTOM){
+            if (this.yMouse + 5 > global.screen_height) {
+                this._onMouseEnter();
+            } else {
+                this._onMouseLeave();
+            }
+        } else if (this.side == St.Side.TOP){
+            if (this.yMouse < 5){
+                this._onMouseEnter();
+            } else {
+                this._onMouseLeave();
+            }
+        }
+        Mainloop.timeout_add(500, Lang.bind(this, this._checkOver));
     },
 
     _onMouseEnter: function(){
-	this.entered = true;
-	Mainloop.timeout_add(1000, Lang.bind(this, this._check));
+        this.entered = true;
+        Mainloop.timeout_add(1000, Lang.bind(this, this._check));
     },
 
     _check: function(){
-	if (this.entered && this.enabled && !this.activated){
-	    this.func();
-	    this.activated = true;
-	}
+        if (this.entered && this.enabled && !this.activated){
+            this.func();
+            this.activated = true;
+        }
     },
 
     _onMouseLeave: function(){
-	this.entered = false;
-	this.activated = false;
+        this.entered = false;
+        this.activated = false;
     }
 };
