b88bac177dca18dc5fa46dfbfdaa66fa072276fb
Per Ångström <per.angstrom@autark.se>
2012-06-24 08:49:17 +0200

    Remove dead function.

--- js/ui/altTab.js
+++ js/ui/altTab.js
@@ -133,36 +133,6 @@ AltTabPopup.prototype = {
         }
     },
 
-    _getAppLists: function() {
-        let tracker = Cinnamon.WindowTracker.get_default();
-        let appSys = Cinnamon.AppSystem.get_default();
-        let allApps = appSys.get_running ();
-
-        let screen = global.screen;
-        let display = screen.get_display();
-        let windows = display.get_tab_list(Meta.TabList.NORMAL, screen,
-                                           screen.get_active_workspace());
-
-        // windows is only the windows on the current workspace. For
-        // each one, if it corresponds to an app we know, move that
-        // app from allApps to apps.
-        let apps = [];
-        for (let i = 0; i < windows.length && allApps.length != 0; i++) {
-            let app = tracker.get_window_app(windows[i]);
-            let index = allApps.indexOf(app);
-            if (index != -1) {
-                apps.push(app);
-                allApps.splice(index, 1);
-            }
-        }
-
-        // Now @apps is a list of apps on the current workspace, in
-        // standard Alt+Tab order (MRU except for minimized windows),
-        // and allApps is a list of apps that only appear on other
-        // workspaces, sorted by user_time, which is good enough.
-        return [apps, allApps];
-    },
-
     show : function(backward, binding, mask) {
         let screen = global.screen;
         let display = screen.get_display();
