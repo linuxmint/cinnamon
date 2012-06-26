5ef44d97ab4932813601e181b8280b09a1b5883b
Per Ångström <per.angstrom@autark.se>
2012-06-26 14:44:56 +0200

    Expo: be more specific about which modifiers are allowed or not when handling delete key press.

--- js/ui/expoThumbnail.js
+++ js/ui/expoThumbnail.js
@@ -760,13 +760,15 @@ ExpoThumbnailsBox.prototype = {
 
     handleKeyPressEvent: function(actor, event) {
         let modifiers = Cinnamon.get_event_state(event);
+        let ctrlAltMask = Clutter.ModifierType.CONTROL_MASK | Clutter.ModifierType.MOD1_MASK;
         let symbol = event.get_key_symbol();
         if (symbol === Clutter.Return || symbol === Clutter.KEY_space) {
             this.activateSelectedWorkspace();
             return true;
         }
-        if ((symbol === Clutter.Delete && (modifiers & Clutter.ModifierType.MODIFIER_MASK) === 0)
-            || symbol === Clutter.w && modifiers & Clutter.ModifierType.CONTROL_MASK) {
+        if ((symbol === Clutter.Delete && (modifiers & ctrlAltMask) !== ctrlAltMask)
+            || symbol === Clutter.w && modifiers & Clutter.ModifierType.CONTROL_MASK)
+        {
             this.removeSelectedWorkspace();
             return true;
         }
