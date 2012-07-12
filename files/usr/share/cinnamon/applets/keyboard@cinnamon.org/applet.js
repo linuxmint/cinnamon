22dc860bc0152a0fa004f34db262ac604e2f7195
Albert Pool <albertpool@solcon.nl>
2012-07-12 10:37:41 +0200

    Keyboard applet updated

--- files/usr/share/cinnamon/applets/keyboard@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/keyboard@cinnamon.org/applet.js
@@ -62,17 +62,15 @@ MyApplet.prototype = {
 
             this._syncConfig();
 
-            if (global.session_type == Cinnamon.SessionType.USER) {
-                this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-                this.menu.addAction(_("Show Keyboard Layout"), Lang.bind(this, function() {
-                    Main.overview.hide();
-                    Util.spawn(['gkbd-keyboard-display', '-g', String(this._config.get_current_group() + 1)]);
-                }));                                
-                this.menu.addAction(_("Show Character Table"), Lang.bind(this, function() {
-                    Main.overview.hide();
-                    Util.spawn(['gucharmap']);
-                }));
-            }
+            this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+            this.menu.addAction(_("Show Keyboard Layout"), Lang.bind(this, function() {
+                Main.overview.hide();
+                Util.spawn(['gkbd-keyboard-display', '-g', String(this._config.get_current_group() + 1)]);
+            }));                                
+            this.menu.addAction(_("Show Character Table"), Lang.bind(this, function() {
+                Main.overview.hide();
+                Util.spawn(['gucharmap']);
+            }));
             this.menu.addSettingsAction(_("Region and Language Settings"), 'gnome-region-panel.desktop'); 
                       
         }
