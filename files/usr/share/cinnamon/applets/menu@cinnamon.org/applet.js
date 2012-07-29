1f9c25a1d2f737a1d20f87699cf58e21cbe5d8fd
Michael Webster <miketwebster@gmail.com>
2012-07-29 10:46:00 -0400

    Fix non-existant style being removed, remove width style in theme that is not needed (originally changed from % to px to remove log errors)

--- files/usr/share/cinnamon/applets/menu@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/menu@cinnamon.org/applet.js
@@ -407,7 +407,7 @@ RecentCategoryButton.prototype = {
         PopupMenu.PopupBaseMenuItem.prototype._init.call(this, {hover: false});
         this.actor.set_style_class_name('menu-category-button');
         this.actor._delegate = this;
-        this.label = new St.Label({ text: _("Recent Files"), style_class: 'menu-place-cat-button-label' });
+        this.label = new St.Label({ text: _("Recent Files"), style_class: 'menu-category-button-label' });
         this.icon = new St.Icon({icon_name: "folder-recent", icon_size: CATEGORY_ICON_SIZE, icon_type: St.IconType.FULLCOLOR});
         this.addActor(this.icon);
         this.addActor(this.label);
