69a294a397eef650642a23dc658fdf5f391735f4
Michael Webster <miketwebster@gmail.com>
2012-09-30 20:14:29 -0400

    window-list:  Add an option in cinnamon-settings to disable alerts in the window-list applet.
    Add pulsing effect to window-list item's icon when it wants
    attention.
    
    Show off-workspace window-list items in the current workspace's
    panel when they want attention.  Clicking on it will switch to
    that window's workspace and focus the window.
    
    Add option to turn off window-list alerts
    
    offscreen notifications

--- files/usr/lib/cinnamon-settings/modules/cs_windows.py
+++ files/usr/lib/cinnamon-settings/modules/cs_windows.py
@@ -38,6 +38,7 @@ class Module:
         sidePage.add_widget(GSettingsCheckButton(_("Enforce displaying the alt-tab switcher on the primary monitor instead of the active one"), "org.cinnamon", "alttab-switcher-enforce-primary-monitor", None))
         sidePage.add_widget(GSettingsCheckButton(_("Enable mouse-wheel scrolling in Window List applet"), "org.cinnamon", "window-list-applet-scroll", None))
         sidePage.add_widget(GSettingsCheckButton(_("Bring windows which require attention to the current workspace (instead of switching to the window's workspace)"), "org.cinnamon", "bring-windows-to-current-workspace", None))
+        sidePage.add_widget(GSettingsCheckButton(_("Enable highlighting and pulsing icon for items in Window List applet that want attention"), "org.cinnamon", "window-list-applet-alert", None))
 
 
 
