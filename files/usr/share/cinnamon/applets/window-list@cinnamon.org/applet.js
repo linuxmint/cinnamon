a6cffbec1ddf18a82adf6ab1fee6979c2ee8cdcd
Michael Webster <miketwebster@gmail.com>
2012-10-20 19:03:35 -0400

    window-list - use temporary alert items and left/right alert containers to handle urgent window alerts

--- files/usr/share/cinnamon/applets/window-list@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/window-list@cinnamon.org/applet.js
@@ -218,15 +218,15 @@ AppMenuButtonRightClickMenu.prototype = {
 
 };
 
-function AppMenuButton(applet, metaWindow, animation, orientation, panel_height) {
-    this._init(applet, metaWindow, animation, orientation, panel_height);
+function AppMenuButton(applet, metaWindow, animation, orientation, panel_height, draggable) {
+    this._init(applet, metaWindow, animation, orientation, panel_height, draggable);
 }
 
 AppMenuButton.prototype = {
 //    __proto__ : AppMenuButton.prototype,
 
     
-    _init: function(applet, metaWindow, animation, orientation, panel_height) {
+    _init: function(applet, metaWindow, animation, orientation, panel_height, draggable) {
                
         this.actor = new St.Bin({ style_class: 'window-list-item-box',
 								  reactive: true,
@@ -245,8 +245,7 @@ AppMenuButton.prototype = {
 
 		this.metaWindow = metaWindow;	
 
-        this._applet = applet;	
-		
+        this._applet = applet;
         let bin = new St.Bin({ name: 'appMenu' });
         this.actor.set_child(bin);
 
@@ -294,31 +293,39 @@ AppMenuButton.prototype = {
 			this.startAnimation(); 
 			this.stopAnimation();
 		}
-		
-        //set up the right click menu
-        this._menuManager = new PopupMenu.PopupMenuManager(this);
-        this.rightClickMenu = new AppMenuButtonRightClickMenu(this.actor, this.metaWindow, orientation);
-        this._menuManager.addMenu(this.rightClickMenu);
-        
+
+
         this._tooltip = new Tooltips.PanelItemTooltip(this, title, orientation);
-        
-        this._draggable = DND.makeDraggable(this.actor);
-        this._draggable.connect('drag-begin', Lang.bind(this, this._onDragBegin));
-        this._draggable.connect('drag-cancelled', Lang.bind(this, this._onDragCancelled));
-        this._draggable.connect('drag-end', Lang.bind(this, this._onDragEnd));
-        
+
+        if (draggable) {
+            //set up the right click menu
+            this._menuManager = new PopupMenu.PopupMenuManager(this);
+            this.rightClickMenu = new AppMenuButtonRightClickMenu(this.actor, this.metaWindow, orientation);
+            this._menuManager.addMenu(this.rightClickMenu);
+
+            this._draggable = DND.makeDraggable(this.actor);
+            this._draggable.connect('drag-begin', Lang.bind(this, this._onDragBegin));
+            this._draggable.connect('drag-cancelled', Lang.bind(this, this._onDragCancelled));
+            this._draggable.connect('drag-end', Lang.bind(this, this._onDragEnd));
+        } else {
+            this._draggable = null;
+        }
+
         this._inEditMode = undefined;
         this.on_panel_edit_mode_changed();
         global.settings.connect('changed::panel-edit-mode', Lang.bind(this, this.on_panel_edit_mode_changed));
         global.settings.connect('changed::window-list-applet-scroll', Lang.bind(this, this.on_scroll_mode_changed));
         this.window_list = this.actor._delegate._applet._windows;
+        this.alert_list = this.actor._delegate._applet._alertWindows;
         this.scroll_connector = null;
         this.on_scroll_mode_changed();
         this._needsAttention = false;
     },
     
     on_panel_edit_mode_changed: function() {
-        this._inEditMode = this._draggable.inhibit = global.settings.get_boolean("panel-edit-mode");
+        if (this._draggable) {
+            this._inEditMode = this._draggable.inhibit = global.settings.get_boolean("panel-edit-mode");
+        }
     }, 
 
     on_scroll_mode_changed: function() {
@@ -409,6 +416,7 @@ AppMenuButton.prototype = {
             this.actor.remove_style_class_name("window-list-item-demands-attention");
             this.actor.remove_style_class_name("window-list-item-demands-attention-top");
             this._needsAttention = false;
+            this._removeAlerts(this.metaWindow);
         } else {
             this.actor.remove_style_pseudo_class('focus');
         }
@@ -449,6 +457,21 @@ AppMenuButton.prototype = {
             }
             Main.activateWindow(this.metaWindow, global.get_current_time());
             this.actor.add_style_pseudo_class('focus');
+            this._removeAlerts(this.metaWindow);
+        }
+    },
+
+    _removeAlerts: function(metaWindow) {
+        for (let i = 0; i < this.alert_list.length; i++) {
+            if (metaWindow == this.alert_list[i].metaWindow) {
+                let alert = this.alert_list[i];
+                if (alert.actor.get_parent()) {
+                    alert.actor.get_parent().remove_actor(alert.actor);
+                }
+                alert.actor.destroy();
+                this.alert_list.splice(i, 1);
+                global.logError("DELETING: "+i);
+            }
         }
     },
 
@@ -604,13 +627,8 @@ AppMenuButton.prototype = {
     getDragActorSource: function() {
         return this.actor;
     },
-<<<<<<< HEAD
-    
-    set_icon: function(panel_height) {  
 
-      let tracker = Cinnamon.WindowTracker.get_default();
-      let app = tracker.get_window_app(this.metaWindow);
-      
+    set_icon: function(panel_height) {
       if (global.settings.get_boolean('panel-scale-text-icons') && global.settings.get_boolean('panel-resizable')) {
         this.iconSize = Math.round(panel_height * ICON_HEIGHT_FACTOR);
       }
@@ -656,7 +674,7 @@ function MyAppletBox(applet) {
 MyAppletBox.prototype = {
     _init: function(applet) {
         this.actor = new St.BoxLayout({ name: 'windowList',
-                                       	style_class: 'window-list-box' });
+                                        style_class: 'window-list-box' });
         this.actor._delegate = this;
         
         this._applet = applet;
@@ -740,6 +758,19 @@ MyAppletBox.prototype = {
     }
 }
 
+function MyAppletAlertBox(applet) {
+    this._init(applet);
+}
+
+MyAppletAlertBox.prototype = {
+    _init: function(applet) {
+        this.actor = new St.BoxLayout({ name: 'windowList',
+                                        style_class: 'window-list-box' });
+        this.actor._delegate = this;
+        this._applet = applet;
+    },
+}
+
 function MyApplet(orientation, panel_height) {
     this._init(orientation, panel_height);
 }
@@ -753,30 +784,22 @@ MyApplet.prototype = {
         try {                    
             this.orientation = orientation;
             this.dragInProgress = false;
-            
+
             this.myactorbox = new MyAppletBox(this);
+            this.leftAlertBox = new MyAppletAlertBox(this);
+            this.rightAlertBox = new MyAppletAlertBox(this);
+
             this.myactor = this.myactorbox.actor;
-        
+
+            this.actor.add(this.leftAlertBox.actor);
             this.actor.add(this.myactor);
+            this.actor.add(this.rightAlertBox.actor);
+
             this.actor.reactive = global.settings.get_boolean("panel-edit-mode");
-                                       	
-            if (orientation == St.Side.TOP) {
-                this.myactor.add_style_class_name('window-list-box-top');
-                this.myactor.set_style('margin-top: 0px;');
-                this.myactor.set_style('padding-top: 0px;');
-                this.actor.set_style('margin-top: 0px;');
-                this.actor.set_style('padding-top: 0px;');
-            }
-            else {
-                this.myactor.add_style_class_name('window-list-box-bottom');
-                this.myactor.set_style('margin-bottom: 0px;');
-                this.myactor.set_style('padding-bottom: 0px;');
-                this.actor.set_style('margin-bottom: 0px;');
-                this.actor.set_style('padding-bottom: 0px;');
-            }
+            this.on_orientation_changed(orientation);
 
             this._windows = new Array();
-
+            this._alertWindows = new Array();
             let tracker = Cinnamon.WindowTracker.get_default();
             tracker.connect('notify::focus-app', Lang.bind(this, this._onFocus));
 
@@ -806,6 +829,28 @@ MyApplet.prototype = {
         }
     },
 
+    on_orientation_changed: function(orientation) {
+        let box_list = this.actor.get_children();
+        if (orientation == St.Side.TOP) {
+            for (let i = 0; i < box_list.length; i++) {
+                box_list[i].add_style_class_name('window-list-box-top');
+                box_list[i].set_style('margin-top: 0px;');
+                box_list[i].set_style('padding-top: 0px;');
+            }
+            this.actor.set_style('margin-top: 0px;');
+            this.actor.set_style('padding-top: 0px;');
+        }
+        else {
+            for (let i = 0; i < box_list.length; i++) {
+                box_list[i].add_style_class_name('window-list-box-bottom');
+                box_list[i].set_style('margin-bottom: 0px;');
+                box_list[i].set_style('padding-bottom: 0px;');
+            }
+            this.actor.set_style('margin-bottom: 0px;');
+            this.actor.set_style('padding-bottom: 0px;');
+        }
+    },
+
     _updateAttentionGrabber: function() {
         let active = global.settings.get_boolean('window-list-applet-alert');
         if (active) {
@@ -818,19 +863,48 @@ MyApplet.prototype = {
     },
 
     on_applet_clicked: function(event) {
-            
     },
 
     on_panel_edit_mode_changed: function() {
         this.actor.reactive = global.settings.get_boolean("panel-edit-mode");
     }, 
-    
-           
+
     _onWindowDemandsAttention : function(display, window) {
         for ( let i=0; i<this._windows.length; ++i ) {
             if ( this._windows[i].metaWindow == window ) {
                 this._windows[i].actor._delegate.getAttention();
-                this._windows[i].actor.show();
+            }
+        }
+        let alertButton = new AppMenuButton(this, window, true, this.orientation, this._panelHeight, false);
+        this._alertWindows.push(alertButton);
+        this.calculate_alert_positions();
+    },
+
+    _clean_alert_boxes: function() {
+        let left_box_items = this.leftAlertBox.actor.get_children();
+        for (let i = 0; i < left_box_items.length; i++) {
+            this.leftAlertBox.actor.remove_actor(left_box_items[i]);
+        }
+        let right_box_items = this.rightAlertBox.actor.get_children();
+        for (let i = 0; i < right_box_items.length; i++) {
+            this.rightAlertBox.actor.remove_actor(right_box_items[i]);
+        }
+    },
+
+    calculate_alert_positions: function() {
+        this._clean_alert_boxes();
+
+        let cur_ws_index = global.screen.get_active_workspace_index();
+
+        for (let i = 0; i < this._alertWindows.length; i++ ) {
+            let window_ws_index = this._alertWindows[i].metaWindow.get_workspace().index();
+            if (window_ws_index < cur_ws_index) {
+                this.leftAlertBox.actor.add(this._alertWindows[i].actor);
+            } else if (window_ws_index > cur_ws_index) {
+                this.rightAlertBox.actor.add(this._alertWindows[i].actor);
+            }
+            if (!this._alertWindows[i]._needsAttention) {
+                this._alertWindows[i].getAttention();
             }
         }
     },
@@ -851,12 +925,12 @@ MyApplet.prototype = {
         for ( let i = 0; i < this._windows.length; ++i ) {
             let metaWindow = this._windows[i].metaWindow;
             if (metaWindow.get_workspace().index() == global.screen.get_active_workspace_index()
-                      || metaWindow.is_on_all_workspaces() || this._windows[i]._needsAttention)
+                      || metaWindow.is_on_all_workspaces())
                 this._windows[i].actor.show();
             else
                 this._windows[i].actor.hide();
         }
-
+        this.calculate_alert_positions();
         this._onFocus();
     },
 
@@ -919,7 +993,7 @@ MyApplet.prototype = {
             }
         }
 
-        let appbutton = new AppMenuButton(this, metaWindow, true, this.orientation, this._panelHeight);
+        let appbutton = new AppMenuButton(this, metaWindow, true, this.orientation, this._panelHeight, true);
         this._windows.push(appbutton);
         this.myactor.add(appbutton.actor);
         if (metaWorkspace.index() != global.screen.get_active_workspace_index())
