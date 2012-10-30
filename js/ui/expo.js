d923e77abe520319937f052cda14f1a4bc37d1af
autarkper <per.angstrom@autark.se>
2012-10-30 19:31:45 +0100

    [Expo] Place dustbin bottom-right.

--- js/ui/expo.js
+++ js/ui/expo.js
@@ -194,7 +194,8 @@ Expo.prototype = {
         if (this._addWorkspaceButton.get_theme_node().get_background_image() == null)
             this._addWorkspaceButton.set_style('background-image: url("/usr/share/cinnamon/theme/add-workspace.png");'); 
 
-        this._windowCloseArea.set_position((primary.width - this._windowCloseArea.width) / 2 , primary.height);
+        // The dustbin is least likely to overlap a thumbnail if placed bottom-right.
+        this._windowCloseArea.set_position(primary.width - this._windowCloseArea.width, primary.height);
         this._windowCloseArea.set_size(this._windowCloseArea.width, this._windowCloseArea.height);
         this._windowCloseArea.raise_top();
     },
