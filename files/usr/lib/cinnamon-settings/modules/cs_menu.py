ba6512fac6ae9b20614d185b21e5b347c9bf0201
superfly1031 <superfly1031@gmail.com>
2013-01-04 17:40:07 -0700

    Update files/usr/lib/cinnamon-settings/modules/cs_menu.py
    Did some organization

--- files/usr/lib/cinnamon-settings/modules/cs_menu.py
+++ files/usr/lib/cinnamon-settings/modules/cs_menu.py
@@ -9,7 +9,9 @@ class Module:
         self.name = "menu"
         sidePage.add_widget(GSettingsEntry(_("Menu text"), "org.cinnamon", "menu-text", None))
         sidePage.add_widget(GSettingsFileChooser(_("Menu icon"), "org.cinnamon", "menu-icon", None, True))
-        sidePage.add_widget(GSettingsSpinButton(_("Menu hover delay"), "org.cinnamon", "menu-hover-delay", None, 0, 2000, 50, 200, _("milliseconds")))
         sidePage.add_widget(GSettingsCheckButton(_("Activate menu on hover"), "org.cinnamon", "activate-menu-applet-on-hover", None))
+        sidePage.add_widget(GSettingsSpinButton(_("Menu hover delay"), "org.cinnamon", "menu-hover-delay", None, 0, 2000, 50, 200, _("milliseconds")))
+            #Need some organization, right?
+            tab.add_widget(Gtk.Separator.new(Gtk.Orientation.HORIZONTAL))
         sidePage.add_widget(GSettingsCheckButton(_("Show bookmarks and places"), "org.cinnamon", "menu-show-places", None))
         sidePage.add_widget(GSettingsCheckButton(_("Show recent files"), "org.cinnamon", "menu-show-recent", None))
