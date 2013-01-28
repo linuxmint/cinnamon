9514117650490963dc1ae948a243f319828d7b52
Lusito <core@roughael.net>
2013-01-28 09:49:40 +0100

    organized menu settings a bit

--- files/usr/lib/cinnamon-settings/modules/cs_menu.py
+++ files/usr/lib/cinnamon-settings/modules/cs_menu.py
@@ -2,6 +2,24 @@
 
 from SettingsWidgets import *
 
+class DoubleListFrame(Gtk.Frame):
+    def __init__(self, label):
+        Gtk.Frame.__init__(self)
+        
+        self.set_label(label)
+        
+        outerbox = Gtk.HBox(True, 0)
+        self.leftBox = Gtk.VBox(True, 0)
+        self.rightBox = Gtk.VBox(True, 0)
+        outerbox.pack_start(self.leftBox, True, True, 5)
+        outerbox.pack_start(self.rightBox, True, True, 5)
+        self.add(outerbox)
+        
+    def add_left(self, widget):
+        self.leftBox.add(widget)
+    def add_right(self, widget):
+        self.rightBox.add(widget)
+
 class Module:
     def __init__(self, content_box):
         sidePage = SidePage(_("Menu"), "menu.svg", content_box)
@@ -11,12 +29,21 @@ class Module:
         sidePage.add_widget(GSettingsFileChooser(_("Menu icon"), "org.cinnamon", "menu-icon", None, True))
         sidePage.add_widget(GSettingsSpinButton(_("Menu hover delay"), "org.cinnamon", "menu-hover-delay", None, 0, 2000, 50, 200, _("milliseconds")))
         sidePage.add_widget(GSettingsCheckButton(_("Activate menu on hover"), "org.cinnamon", "activate-menu-applet-on-hover", None))
-        sidePage.add_widget(GSettingsCheckButton(_("Show bookmarks and places"), "org.cinnamon", "menu-show-places", None))
-        sidePage.add_widget(GSettingsCheckButton(_("Show recent files"), "org.cinnamon", "menu-show-recent", None))
-        sidePage.add_widget(GSettingsCheckButton(_("Show favorites"), "org.cinnamon", "menu-show-favorites", None))
-        sidePage.add_widget(GSettingsCheckButton(_("Show system buttons"), "org.cinnamon", "menu-show-system-buttons", None))
-        sidePage.add_widget(GSettingsCheckButton(_("Show app-info title"), "org.cinnamon", "menu-show-appinfo-title", None))
-        sidePage.add_widget(GSettingsCheckButton(_("Show app-info description"), "org.cinnamon", "menu-show-appinfo-description", None))
-        sidePage.add_widget(GSettingsCheckButton(_("Use multiple lines for app-info description"), "org.cinnamon", "menu-use-multiline-appinfo", None))
-        sidePage.add_widget(GSettingsCheckButton(_("Align app-info right"), "org.cinnamon", "menu-align-appinfo-right", None))
-        sidePage.add_widget(GSettingsCheckButton(_("Flip searchbox and app-info"), "org.cinnamon", "menu-flip-search-appinfo", None))
+        
+        visibleItemsFrame = DoubleListFrame(_("Visible Items"))
+        sidePage.add_widget(visibleItemsFrame)
+        
+        visibleItemsFrame.add_left(GSettingsCheckButton(_("Show bookmarks and places"), "org.cinnamon", "menu-show-places", None))
+        visibleItemsFrame.add_left(GSettingsCheckButton(_("Show recent files"), "org.cinnamon", "menu-show-recent", None))
+        visibleItemsFrame.add_right(GSettingsCheckButton(_("Show favorites"), "org.cinnamon", "menu-show-favorites", None))
+        visibleItemsFrame.add_right(GSettingsCheckButton(_("Show system buttons"), "org.cinnamon", "menu-show-system-buttons", None))
+        
+        appInfoFrame = DoubleListFrame(_("Selected Application Information"))
+        sidePage.add_widget(appInfoFrame)
+        
+        appInfoFrame.add_left(GSettingsCheckButton(_("Show title"), "org.cinnamon", "menu-show-appinfo-title", None))
+        appInfoFrame.add_left(GSettingsCheckButton(_("Show description"), "org.cinnamon", "menu-show-appinfo-description", None))
+        appInfoFrame.add_right(GSettingsCheckButton(_("Align title and description right"), "org.cinnamon", "menu-align-appinfo-right", None))
+        appInfoFrame.add_right(GSettingsCheckButton(_("Use multiple lines for description"), "org.cinnamon", "menu-use-multiline-appinfo", None))
+
+        sidePage.add_widget(GSettingsCheckButton(_("Flip searchbox and selected application information"), "org.cinnamon", "menu-flip-search-appinfo", None))
