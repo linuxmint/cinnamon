92b6bdb6626dcd57c9647868deb30bc853ebceee
autarkper <per.angstrom@autark.se>
2013-02-11 17:50:46 +0100

    [Connector] Code review
    1) Rename makeConnection -> connect
    2) Add a return value to Connector.addConnection
    3) A few minor additional changes.

--- js/misc/connector.js
+++ js/misc/connector.js
@@ -1,51 +1,68 @@
 // -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
 
-/* usage: "makeConnection(someObject, 'some-signal', someFunction [, ...])"
+/* usage:
+ * "let connection = connect(someObject, 'some-signal', someFunction [, ...])
+ *  ///...
+ *  connection.disconnect();
+ *  "
  * 
  * @arg-0: target, the object you want to connect to
  * @arg-1 .. @arg-n: arguments to the target's connect function
  *
  * return value: an object that you call disconnect on
  */
-var makeConnection = function() {
+var connect = function() {
     let args = [].slice.apply(arguments);
     let target = args.shift();
     let id = target.connect.apply(target, args);
     return {
-        disconnect: function() {if (target) {target.disconnect(id); target = null;}},
-        forget: function() {target = null;},
-        getTarget: function() {return target;}
+        disconnect: function() {
+            if (target) {
+                target.disconnect(id); target = null;
+                }
+        },
+        forget: function() {
+            target = null;
+        },
+        getTarget: function() {
+            return target;
+        }
     };
 };
 
 function Connector() {
-    this._init(arguments);
+    this._init.apply(this, arguments);
 }
 
 /* A class that takes care of your connections - just remember to
  * call destroy when it is time to disconnect.
  */
 Connector.prototype = {
-_init: function() {
-    this.connections = [];
+    _init: function() {
+        this.connections = [];
+    },
 
     /* usage: "addConnection(someObject, 'some-signal', someFunction [, ...])"
      * 
      * @arg-0: target, the object you want to connect to
      * @arg-1 .. @arg-n: arguments to the target's connect function
+     *
+     * @return aConnection, the created connection, which you can optionally disconnect or "forget" later on.
      */
-    this.addConnection = function() {
-        let connection = makeConnection.apply(0, arguments);
+    addConnection: function() {
+        let connection = connect.apply(0, arguments);
         this.connections.push(connection);
-    };
+        return connection;
+    },
 
     /* Disconnects all connections.
      */
-    this.destroy = function() {
-        this.connections.forEach(function(connection) {
-            connection.disconnect();
-        }, this);
-        this.connections = null;
-    };
-}
+    destroy: function() {
+        if (this.connections) {
+            this.connections.forEach(function(connection) {
+                connection.disconnect();
+            }, this);
+            this.connections = null;
+        }
+    }
 };
