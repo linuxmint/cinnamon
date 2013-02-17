24bb8872f6ff583dd6311c455bb1316e71c2592a
Lusito <core@roughael.net>
2013-02-17 18:27:47 +0100

    Menu Overhaul clean recommit

--- files/usr/lib/cinnamon-settings/modules/cs_menu.py
+++ files/usr/lib/cinnamon-settings/modules/cs_menu.py
@@ -2,6 +2,24 @@
 
 from SettingsWidgets import *
 
+class DoubleListFrame(Gtk.Frame):
+    def __init__(self, label):
+        Gtk.Frame.__init__(self)
+        
+        self.set_label(label)
+        
+        outerbox = Gtk.HBox(True, 0)
+        self.leftBox = Gtk.VBox(True, 0)
+        self.rightBox = Gtk.VBox(True, 0)
+        outerbox.pack_start(self.leftBox, True, True, 5)
+        outerbox.pack_start(self.rightBox, True, True, 5)
+        self.add(outerbox)
+        
+    def add_left(self, widget):
+        self.leftBox.add(widget)
+    def add_right(self, widget):
+        self.rightBox.add(widget)
+
 class Module:
     def __init__(self, content_box):
         keywords = _("menu, start, bookmarks, places, recent")
@@ -10,14 +28,18 @@ class Module:
         self.sidePage = sidePage
         self.name = "menu"
         self.category = "prefs"
+        sidePage.add_widget(GSettingsEntry(_("Menu text"), "org.cinnamon", "menu-text", None))
+        sidePage.add_widget(GSettingsFileChooser(_("Menu icon"), "org.cinnamon", "menu-icon", None, True))
+        sidePage.add_widget(GSettingsSpinButton(_("Menu hover delay"), "org.cinnamon", "menu-hover-delay", None, 0, 2000, 50, 200, _("milliseconds")))
+        sidePage.add_widget(GSettingsCheckButton(_("Activate menu on hover"), "org.cinnamon", "activate-menu-applet-on-hover", None))
         
+        visibleItemsFrame = DoubleListFrame(_("Visible Items"))
+        sidePage.add_widget(visibleItemsFrame)
         
-        sidePage.add_widget(GSettingsFileChooser(_("Icon"), "org.cinnamon", "menu-icon", None, True))
-        sidePage.add_widget(GSettingsEntry(_("Text"), "org.cinnamon", "menu-text", None))
-                
-        sidePage.add_widget(GSettingsCheckButton(_("Show bookmarks and places"), "org.cinnamon", "menu-show-places", None))
-        sidePage.add_widget(GSettingsCheckButton(_("Show recent files"), "org.cinnamon", "menu-show-recent", None))
-                
-        sidePage.add_widget(GSettingsCheckButton(_("Open menu when I move my mouse over it"), "org.cinnamon", "activate-menu-applet-on-hover", None), True)
-        sidePage.add_widget(GSettingsCheckButton(_("Enable filesystem path entry in search box"), "org.cinnamon", "menu-search-filesystem", None), True)
-        sidePage.add_widget(GSettingsSpinButton(_("Menu hover delay"), "org.cinnamon", "menu-hover-delay", None, 0, 2000, 50, 200, _("milliseconds")), True)
+        visibleItemsFrame.add_left(GSettingsCheckButton(_("Bookmarks and places"), "org.cinnamon", "menu-show-places", None))
+        visibleItemsFrame.add_left(GSettingsCheckButton(_("Recent files"), "org.cinnamon", "menu-show-recent", None))
+        visibleItemsFrame.add_right(GSettingsCheckButton(_("Favorites"), "org.cinnamon", "menu-show-favorites", None))
+        visibleItemsFrame.add_right(GSettingsCheckButton(_("System buttons"), "org.cinnamon", "menu-show-system-buttons", None))
+        visibleItemsFrame.add_left(GSettingsCheckButton(_("Selected application title"), "org.cinnamon", "menu-show-appinfo-title", None))
+        visibleItemsFrame.add_right(GSettingsCheckButton(_("Selected application description"), "org.cinnamon", "menu-show-appinfo-description", None))
+
