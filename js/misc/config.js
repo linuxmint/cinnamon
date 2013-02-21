06803315b6b984d11300f0de8c70d3ffaff2ec3f
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2013-02-21 16:23:45 +0000

    1.7.1

--- js/misc/config.js
+++ js/misc/config.js
@@ -3,7 +3,7 @@
 /* The name of this package (not localized) */
 const PACKAGE_NAME = 'cinnamon';
 /* The version of this package */
-const PACKAGE_VERSION = '1.6.7';
+const PACKAGE_VERSION = '1.7.1';
 /* The version of GJS we're linking to */
 const GJS_VERSION = '1.32.0';
 /* 1 if gnome-bluetooth is available, 0 otherwise */
