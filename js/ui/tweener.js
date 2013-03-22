e8c18a0b31cdb042153701bef30df15d5de3230a
Corey Farrell <lion@cfware.com>
2013-03-22 04:27:00 -0400

    * set tweener animation time to 0.001 seconds when desktop effects are disabled.  0 cannot be used as it prevents callbacks from functioning. * add support for FORCE_ANIMATION boolean parameter to enable the animation even when desktop effects are disabled.

--- js/ui/tweener.js
+++ js/ui/tweener.js
@@ -60,6 +60,8 @@ function addTween(target, tweeningParameters) {
 }
 
 function _wrapTweening(target, tweeningParameters) {
+    if (!tweeningParameters.FORCE_ANIMATION && !global.settings.get_boolean('desktop-effects'))
+        tweeningParameters.time = 0.001;
     let state = _getTweenState(target);
 
     if (!state.destroyedId) {
