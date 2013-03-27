8b504177c65d09b6797175bf53d3d6a8e3447613
dalcde <dalcde@yahoo.com.hk>
2013-03-27 20:23:25 +0800

    [Main] Use Date.toISOString in formatTime function

--- js/ui/main.js
+++ js/ui/main.js
@@ -805,13 +805,7 @@ function _logInfo(msg) {
 }
 
 function formatTime(d) {
-    function pad(n) { return n < 10 ? '0' + n : n; }
-    return d.getUTCFullYear()+'-'
-        + pad(d.getUTCMonth()+1)+'-'
-        + pad(d.getUTCDate())+'T'
-        + pad(d.getUTCHours())+':'
-        + pad(d.getUTCMinutes())+':'
-        + pad(d.getUTCSeconds())+'Z';
+    return d.toISOString();
 }
 
 function renderLogLine(line) {
