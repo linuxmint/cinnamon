eca12da9fc42da20a75627ef4e803bd8fafb84f8
Michael Webster <miketwebster@gmail.com>
2013-04-01 15:55:07 -0400

    keybindings.js: change xlet to 'custom'

--- js/ui/keybindings.js
+++ js/ui/keybindings.js
@@ -12,9 +12,9 @@ KeybindingManager.prototype = {
 
     addHotKey: function(name, binding, callback) {
         if (this.bindings[name]) {
-            global.display.remove_xlet_keybinding(name);
+            global.display.remove_custom_keybinding(name);
         }
-        if (!global.display.add_xlet_keybinding(name, binding, callback)) {
+        if (!global.display.add_custom_keybinding(name, binding, callback)) {
             global.logError("Warning, unable to bind hotkey with name '" + name + "'.  The selected keybinding could already be in use.");
             return false;
         } else {
@@ -25,7 +25,7 @@ KeybindingManager.prototype = {
     },
 
     removeHotKey: function(name) {
-        global.display.remove_xlet_keybinding(name);
+        global.display.remove_custom_keybinding(name);
         global.display.rebuild_keybindings();
         this.bindings[name] = undefined;
     }
