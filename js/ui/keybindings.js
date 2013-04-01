df622c7ad0d5df99fb3d070491f2e2f62bae2452
Michael Webster <miketwebster@gmail.com>
2013-04-01 18:33:18 -0400

    Improve keybinding setter widget for applet settings.  Properly handles modifier keys across all platforms instead of relying on Ubuntu-specific code for special modifier key handling.

--- js/ui/keybindings.js
+++ js/ui/keybindings.js
@@ -14,8 +14,17 @@ KeybindingManager.prototype = {
         if (this.bindings[name]) {
             global.display.remove_custom_keybinding(name);
         }
+        if (binding == "") {
+            global.logError("Empty keybinding set for " + name + ", ignoring");
+            if (this.bindings[name]) {
+                this.bindings[name] = undefined;
+            }
+            global.display.rebuild_keybindings();
+            return true;
+        }
         if (!global.display.add_custom_keybinding(name, binding, callback)) {
             global.logError("Warning, unable to bind hotkey with name '" + name + "'.  The selected keybinding could already be in use.");
+            global.display.rebuild_keybindings();
             return false;
         } else {
             this.bindings[name] = binding;
