6fb25947d2f2714776a7b12fd3971281a927a216
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2013-04-24 00:40:15 +0100

    Changed a few icons in cinnamon-settings

--- files/usr/lib/cinnamon-settings/modules/cs_default.py
+++ files/usr/lib/cinnamon-settings/modules/cs_default.py
@@ -301,7 +301,7 @@ class Module:
     def __init__(self, content_box):
         keywords = _("media, defaults, applications, programs, removable, browser, email, calendar, music, videos, photos, images, cd, autostart")
         advanced = False
-        sidePage = SidePage(_("Applications & Removable Media"), "details.svg", keywords, advanced, content_box)
+        sidePage = SidePage(_("Applications & Removable Media"), "default-applications.svg", keywords, advanced, content_box)
         self.sidePage = sidePage
         self.name = "default"
         self.category = "prefs"
