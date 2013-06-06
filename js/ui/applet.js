0059d1627102425020a136c025ebbff9c5d59591
Lusito <core@roughael.net>
2013-06-06 15:52:39 +0200

    Connector recommit

--- js/ui/applet.js
+++ js/ui/applet.js
@@ -8,6 +8,7 @@ const DND = imports.ui.dnd;
 const Clutter = imports.gi.Clutter;
 const AppletManager = imports.ui.appletManager;
 const Gtk = imports.gi.Gtk;
+const Connector = imports.misc.connector;
 const Util = imports.misc.util;
 const Pango = imports.gi.Pango;
 const Mainloop = imports.mainloop;
@@ -174,7 +175,7 @@ Applet.prototype = {
     _init: function(orientation, panel_height, instance_id) {
         this.actor = new St.BoxLayout({ style_class: 'applet-box', reactive: true, track_hover: true });        
         this._applet_tooltip = new Tooltips.PanelItemTooltip(this, "", orientation);                                        
-        this.actor.connect('button-release-event', Lang.bind(this, this._onButtonReleaseEvent));  
+        this.createConnection(this.actor, 'button-release-event', Lang.bind(this, this._onButtonReleaseEvent));  
 
         this._menuManager = new PopupMenu.PopupMenuManager(this);
         this._applet_context_menu = new AppletContextMenu(this, orientation);
@@ -193,9 +194,9 @@ Applet.prototype = {
         this._hook = null; // Defined in metadata.json, set by appletManager
         this._dragging = false;                
         this._draggable = DND.makeDraggable(this.actor);
-        this._draggable.connect('drag-begin', Lang.bind(this, this._onDragBegin));
-    	this._draggable.connect('drag-cancelled', Lang.bind(this, this._onDragCancelled));
-        this._draggable.connect('drag-end', Lang.bind(this, this._onDragEnd));        
+        this.createConnection(this._draggable, 'drag-begin', Lang.bind(this, this._onDragBegin));
+    	this.createConnection(this._draggable, 'drag-cancelled', Lang.bind(this, this._onDragCancelled));
+        this.createConnection(this._draggable, 'drag-end', Lang.bind(this, this._onDragEnd));        
 
         this._scaleMode = false;
         this._applet_tooltip_text = "";
@@ -204,7 +205,7 @@ Applet.prototype = {
         this.context_menu_separator = null;
 
         this._setAppletReactivity();
-        this._panelEditModeChangedId = global.settings.connect('changed::panel-edit-mode', Lang.bind(this, function() {
+        this.createConnection(global.settings, 'changed::panel-edit-mode', Lang.bind(this, function() {
             this._setAppletReactivity();
             this.finalizeContextMenu();
         }));
@@ -315,7 +316,7 @@ Applet.prototype = {
 
     // should only be called by appletManager
     _onAppletRemovedFromPanel: function() {
-        global.settings.disconnect(this._panelEditModeChangedId);
+        this.destroyConnections();
         this.on_applet_removed_from_panel();
     },
 
@@ -414,6 +415,8 @@ Applet.prototype = {
         }
     }
 };
+
+Connector.addConnectorMethods(Applet.prototype);
 
 /**
  * #IconApplet:
