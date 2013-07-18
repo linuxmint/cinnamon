dbc41558d087a41cf0dea404bcbe45365105906a
Michael Webster <miketwebster@gmail.com>
2013-07-18 19:14:26 -0400

    Add some more keybindings

--- js/ui/keybindings.js
+++ js/ui/keybindings.js
@@ -16,16 +16,37 @@ const CUSTOM_KEYS_SCHEMA = "org.cinnamon.keybindings.custom-keybinding";
 const MEDIA_KEYS_SCHEMA = "org.cinnamon.settings-daemon.plugins.media-keys"
 
 /* Enums and gsettings key names - enums for ref, to match up with
-   cinnamon-settings-daemon shortcuts-list.h */
+   cinnamon-settings-daemon shortcuts-list.h
+
+   value: enum value, of MediaKeyType
+   key: gsettings key (in media-keys schema)
+   kb: hardcoded keybinding
+
+   key and kb are mutually exclusive, as in shortcuts-list.h
+   Eventually maybe hardcoded bindings should be configurable
+
+   We don't need everything here, just items we may want to access
+   while a modal cinnamon dialog is open, otherwise we're better off
+   just having c-s-d handling it.
+*/
+
 var MediaKeyTypes = {
-  MUTE_KEY            : {value: 3,  key: "volume-mute"}, 
-  VOLUME_DOWN_KEY     : {value: 4,  key: "volume-down"}, 
-  VOLUME_UP_KEY       : {value: 5,  key: "volume-up"},
-  PLAY_KEY            : {value: 26, key: "play"},
-  PAUSE_KEY           : {value: 27, key: "pause"},
-  STOP_KEY            : {value: 28, key: "stop"},
-  PREVIOUS_KEY        : {value: 29, key: "previous"},
-  NEXT_KEY            : {value: 30, key: "next"}
+  MUTE_KEY                    : {value: 3,  key: "volume-mute",             kb: null                    },
+  VOLUME_DOWN_KEY             : {value: 4,  key: "volume-down",             kb: null                    },
+  VOLUME_UP_KEY               : {value: 5,  key: "volume-up",               kb: null                    },
+  EJECT_KEY                   : {value: 10, key: "eject",                   kb: null                    },
+  MEDIA_KEY                   : {value: 12, key: "media",                   kb: null                    },
+  SCREENSHOT_KEY              : {value: 18, key: "screenshot",              kb: null                    },
+  WINDOW_SCREENSHOT_KEY       : {value: 19, key: "window-screenshot",       kb: null                    },
+  PLAY_KEY                    : {value: 26, key: "play",                    kb: null                    },
+  PAUSE_KEY                   : {value: 27, key: "pause",                   kb: null                    },
+  STOP_KEY                    : {value: 28, key: "stop",                    kb: null                    },
+  PREVIOUS_KEY                : {value: 29, key: "previous",                kb: null                    },
+  NEXT_KEY                    : {value: 30, key: "next",                    kb: null                    },
+  REWIND_KEY                  : {value: 31, key: null,                      kb: "XF86AudioRewind"       },
+  FORWARD_KEY                 : {value: 32, key: null,                      kb: "XF86AudioForward"      },
+  REPEAT_KEY                  : {value: 33, key: null,                      kb: "XF86AudioRepeat"       },
+  RANDOM_KEY                  : {value: 34, key: null,                      kb: "XF86AudioRandomPlay"   }
 };
 
 const MediaKeysManagerInterface = {
@@ -116,10 +137,19 @@ KeybindingManager.prototype = {
 
     setup_media_keys: function() {
         for (let type in MediaKeyTypes) {
-            let keybinding = this.csd_kb_settings.get_string(MediaKeyTypes[type].key);
-            this.addHotKey("settings-daemon-" + MediaKeyTypes[type].key,
-                           keybinding,
-                           Lang.bind(this, this.on_media_key_pressed, MediaKeyTypes[type].value));
+            if (MediaKeyTypes[type].key) {
+                let keybinding = this.csd_kb_settings.get_string(MediaKeyTypes[type].key);
+                this.addHotKey("settings-daemon-" + type.toString(),
+                               keybinding,
+                               Lang.bind(this, this.on_media_key_pressed, MediaKeyTypes[type].value));
+            } else if (MediaKeyTypes[type].kb) {
+                let keybinding = MediaKeyTypes[type].kb;
+                this.addHotKey("settings-daemon-" + type.toString(),
+                               keybinding,
+                               Lang.bind(this, this.on_media_key_pressed, MediaKeyTypes[type].value));
+            } else {
+                global.logError("Invalid media-keys keybinding: " + type.toString());
+            }
         }
     },
 
