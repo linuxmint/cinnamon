e75c4bf9ab13df49929ad3650f607c67fecc20e5
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2013-10-08 14:10:02 +0100

    Removed icon from Edit Menu item in menu applet

--- files/usr/share/cinnamon/applets/menu@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/menu@cinnamon.org/applet.js
@@ -865,7 +865,7 @@ MyApplet.prototype = {
             Main.placesManager.connect('places-updated', Lang.bind(this, this._refreshPlacesAndRecent));
             this.RecentManager.connect('changed', Lang.bind(this, this._refreshPlacesAndRecent));
 
-            this.edit_menu_item = new Applet.MenuItem(_("Edit menu"), Gtk.STOCK_EDIT, Lang.bind(this, this._launch_editor));
+            this.edit_menu_item = new Applet.MenuItem(_("Edit menu"), null, Lang.bind(this, this._launch_editor));
             this._applet_context_menu.addMenuItem(this.edit_menu_item);
             let settings_menu_item = new Applet.MenuItem(_("Menu settings"), null, function() {
                 Util.spawnCommandLine("cinnamon-settings menu");
