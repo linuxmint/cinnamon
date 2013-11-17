3b26b9c2d38d2aaa06934e2bd6fbf3a346b7279f
Michael Webster <miketwebster@gmail.com>
2013-11-16 23:55:24 -0500

    Fix default applications pickers (audio, video, images) to set default for all related mimetypes they support

--- files/usr/lib/cinnamon-settings/modules/cs_default.py
+++ files/usr/lib/cinnamon-settings/modules/cs_default.py
@@ -13,20 +13,27 @@ CUSTOM_ITEM_OPEN_FOLDER = "cc-item-open-folder"
 
 MEDIA_HANDLING_SCHEMA = "org.cinnamon.desktop.media-handling"
 
+PREF_CONTENT_TYPE = 0
+PREF_GEN_CONTENT_TYPE = 1
+PREF_LABEL = 2
+
 DEF_CONTENT_TYPE = 0
 DEF_LABEL = 1
 DEF_HEADING = 2
-        
+
 preferred_app_defs = [
     # for web, we need to support text/html,
     # application/xhtml+xml and x-scheme-handler/https,
     # hence the "*" pattern
-    ( "x-scheme-handler/http",      _("_Web") ),
-    ( "x-scheme-handler/mailto",    _("_Mail") ),
-    ( "text/calendar",              _("_Calendar") ),
-    ( "audio/x-vorbis+ogg",         _("M_usic") ),
-    ( "video/x-ogm+ogg",            _("_Video") ),
-    ( "image/jpeg",                 _("_Photos") )
+    ( "x-scheme-handler/http",   "x-scheme-handler/http",      _("_Web") ),
+    ( "x-scheme-handler/mailto", "x-scheme-handler/mailto",    _("_Mail") ),
+    ( "text/calendar",           "text/calendar",              _("_Calendar") ),
+
+    # 1st mimetype is to let us find apps
+    # 2nd mimetype is to set default handler for (so we handle all of that type, not just a specific format)
+    ( "audio/x-vorbis+ogg",      "audio",                    _("M_usic") ),
+    ( "video/x-ogm+ogg",         "video",                    _("_Video") ),
+    ( "image/jpeg",              "image",                    _("_Photos") )
 ]
 
 removable_media_defs = [
@@ -97,23 +104,25 @@ class MnemonicLabel(Gtk.Label):
         self.set_mnemonic_widget(widget)
 
 class DefaultAppChooserButton(Gtk.AppChooserButton):
-    def __init__(self, content_type):
+    def __init__(self, content_type, gen_content_type):
         super(DefaultAppChooserButton, self).__init__(content_type=content_type)
         self.content_type = content_type
-
-        self.connect("changed", self.onChanged)
+        self.generic_content_type = gen_content_type
         self.set_show_default_item(True)
-        
+        self.connect("changed", self.onChanged)
+
     def onChanged(self, button):
         info = button.get_app_info()
         if info:
-            if not info.set_as_default_for_type(self.content_type):
-                print "Failed to set '%s' as the default application for '%s'" % (info.get_name(), self.content_type)
-
+            types = info.get_supported_types()
+            for t in types:
+                if self.generic_content_type in t:
+                    if not info.set_as_default_for_type(t):
+                        print "Failed to set '%s' as the default application for '%s'" % (info.get_name(), self.generic_content_type)
             if self.content_type == "x-scheme-handler/http":
                 if info.set_as_default_for_type ("x-scheme-handler/https") == False:
                     print "Failed to set '%s' as the default application for '%s'" % (info.get_name(), "x-scheme-handler/https")
-        
+
 class CustomAppChooserButton(Gtk.AppChooserButton):
     def __init__(self, media_settings, content_type, heading=None):
         super(CustomAppChooserButton, self).__init__(content_type=content_type)
@@ -316,8 +325,8 @@ class Module:
         table = ButtonTable(len(preferred_app_defs))
         
         for d in preferred_app_defs:
-            table.addRow(d[DEF_LABEL], DefaultAppChooserButton(d[DEF_CONTENT_TYPE]))
-        
+            table.addRow(d[PREF_LABEL], DefaultAppChooserButton(d[PREF_CONTENT_TYPE], d[PREF_GEN_CONTENT_TYPE]))
+
         return ColumnBox(_("Default Applications"), table)
 
     def onMoreClicked(self, button):
