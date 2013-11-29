525742d6132ffa9dbd6ba91411c2201ea4df0aa0
Michael Webster <miketwebster@gmail.com>
2013-11-29 10:46:26 -0500

    menu applet:  Back to using texturecache directly for icons
    TODO: look into making St.Icon faster

--- files/usr/share/cinnamon/applets/menu@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/menu@cinnamon.org/applet.js
@@ -352,14 +352,7 @@ ApplicationButton.prototype = {
         this.category = new Array();
         this.actor.set_style_class_name('menu-application-button');
 
-        this.icon = null;                
-        if (this.app.get_app_info() != null && this.app.get_app_info().get_icon() != null) {
-            this.icon = new St.Icon({gicon: this.app.get_app_info().get_icon(), icon_size: APPLICATION_ICON_SIZE, icon_type: St.IconType.FULLCOLOR});
-        }
-        if (this.icon == null) {
-            this.icon = new St.Icon({icon_name: "application-x-executable", icon_size: APPLICATION_ICON_SIZE, icon_type: St.IconType.FULLCOLOR});        
-        }
-
+        this.icon = this.app.create_icon_texture(APPLICATION_ICON_SIZE)
         this.addActor(this.icon);
         this.name = this.app.get_name();
         this.label = new St.Label({ text: this.name, style_class: 'menu-application-button-label' });
@@ -506,8 +499,9 @@ CategoryButton.prototype = {
 
         this.actor.set_style_class_name('menu-category-button');
         var label;
+        let icon = null;
         if (category) {
-            let icon = category.get_icon();
+            icon = category.get_icon();
             if (icon && icon.get_names)
                 this.icon_name = icon.get_names().toString();
             else
@@ -519,7 +513,7 @@ CategoryButton.prototype = {
         this.actor._delegate = this;
         this.label = new St.Label({ text: label, style_class: 'menu-category-button-label' });
         if (category && this.icon_name) {
-            this.icon = new St.Icon({icon_name: this.icon_name, icon_size: CATEGORY_ICON_SIZE, icon_type: St.IconType.FULLCOLOR});
+            this.icon = St.TextureCache.get_default().load_gicon(null, icon, CATEGORY_ICON_SIZE);
             this.addActor(this.icon);
             this.icon.realize();
         }
@@ -584,14 +578,7 @@ FavoritesButton.prototype = {
         this.actor.style = "padding-top: "+(icon_size/3)+"px;padding-bottom: "+(icon_size/3)+"px; margin:auto;"
 
         this.actor.add_style_class_name('menu-favorites-button');    
-
-        let icon = null;                
-        if (app.get_app_info() != null && app.get_app_info().get_icon() != null) {
-            icon = new St.Icon({gicon: app.get_app_info().get_icon(), icon_size: icon_size, icon_type: St.IconType.FULLCOLOR});
-        }
-        if (icon == null) {
-            icon = new St.Icon({icon_name: "application-x-executable", icon_size: icon_size, icon_type: St.IconType.FULLCOLOR});        
-        }
+        let icon = app.create_icon_texture(icon_size);
 
         this.addActor(icon);
         icon.realize()
@@ -885,10 +872,18 @@ MyApplet.prototype = {
             this.lastAcResults = new Array();
 
             this.settings.bindProperty(Settings.BindingDirection.IN, "search-filesystem", "searchFilesystem", null, null);
+
+            St.TextureCache.get_default().connect("icon-theme-changed", Lang.bind(this, this.onIconThemeChanged));
         }
         catch (e) {
             global.logError(e);
         }
+    },
+
+    onIconThemeChanged: function() {
+        this._refreshApps();
+        this._refreshFavs();
+        this._refreshPlacesAndRecent;
     },
 
     openMenu: function() {
