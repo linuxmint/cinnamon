f2bffbf7cac96e7b58039685d18d3f9a41f4f6fe
Gilles De Mey <gilles.de.mey@me.com>
2013-12-28 18:59:18 +0100

    Adding high resolution cover art for Spotify
    All we need to do is change the original url (http://open.spotify.com/thumb/<hash>) to one the has one of the following sizes: 60, 85, 120, 300, or 640 so it becomes http://open.spotify.com/<size>/<hash>
    
    300 seemed most appropriate for the Sound applet.
    
    As an additional bonus, it also removes the Spotify watermark.

--- files/usr/share/cinnamon/applets/sound@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/sound@cinnamon.org/applet.js
@@ -590,6 +590,10 @@ Player.prototype = {
         if (metadata["mpris:artUrl"]) {
             if (this._trackCoverFile != metadata["mpris:artUrl"].toString()) {
                 this._trackCoverFile = metadata["mpris:artUrl"].toString();
+                
+                if ( this._name === "spotify" )
+                    this._trackCoverFile = this._trackCoverFile.replace("/thumb/", "/300/");
+
                 change = true;
             }
         }
