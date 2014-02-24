0c4acbec226cd0cedd9544212923b99e0eb5604e
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2014-02-24 10:55:45 +0000

    Improved the layout of the screen module

--- files/usr/lib/cinnamon-settings/modules/cs_screensaver.py
+++ files/usr/lib/cinnamon-settings/modules/cs_screensaver.py
@@ -4,15 +4,15 @@ import os
 from SettingsWidgets import *
 
 LOCK_DELAY_OPTIONS = [
-    (0, _("When the screen turns off")),
-    (30, _("30 seconds after the screen turns off")),
-    (60, _("1 minute after the screen turns off")),
-    (120, _("2 minutes after the screen turns off")),
-    (180, _("3 minutes after the screen turns off")),
-    (300, _("5 minutes after the screen turns off")),
-    (600, _("10 minutes after the screen turns off")),
-    (1800, _("30 minutes after the screen turns off")),
-    (3600, _("1 hour after the screen turns off"))
+    (0, _("Immediately")),
+    (30, _("After 30 seconds")),
+    (60, _("After 1 minute")),
+    (120, _("After 2 minutes")),
+    (180, _("After 3 minutes")),
+    (300, _("After 5 minutes")),
+    (600, _("After 10 minutes")),
+    (1800, _("After 30 minutes")),
+    (3600, _("After 1 hour"))
 ]
 
 class Module:
@@ -24,26 +24,45 @@ class Module:
         self.name = "screensaver"
         self.category = "prefs"
         self.comment = _("Manage screen lock settings")
-       
-        frame_label = Gtk.Label()
-        frame_label.set_markup("<b>%s</b>" % _("Activation"))
-        frame = Gtk.Frame()
-        frame.set_label_widget(frame_label)
-        frame.set_shadow_type(Gtk.ShadowType.NONE)
-        vbox = Gtk.VBox()
-        vbox.pack_start(GSettingsCheckButton(_("Lock the screen automatically"), "org.cinnamon.desktop.screensaver", "lock-enabled", None), False, False, 2)
-        vbox.pack_start(GSettingsIntComboBox(_(""), "org.cinnamon.desktop.screensaver", "lock-delay", "org.cinnamon.desktop.screensaver/lock-enabled", LOCK_DELAY_OPTIONS, use_uint=True), False, False, 2)
-        vbox.pack_start(GSettingsCheckButton(_("Lock the screen before suspending the computer"), "org.cinnamon.settings-daemon.plugins.power", "lock-on-suspend", None), False, False, 2)
-        frame.add(vbox)
-        self.sidePage.add_widget(frame)
 
-        frame_label = Gtk.Label()
-        frame_label.set_markup("<b>%s</b>" % _("Away message"))
-        frame = Gtk.Frame()
-        frame.set_label_widget(frame_label)
-        frame.set_shadow_type(Gtk.ShadowType.NONE)
-        vbox = Gtk.VBox()
-        vbox.pack_start(GSettingsCheckButton(_("Ask for an away message when locking the screen from the menu"), "org.cinnamon.screensaver", "ask-for-away-message", None), False, False, 2)
-        vbox.pack_start(GSettingsEntry(_("Default away message"), "org.cinnamon.screensaver", "default-message", None), False, False, 2)
-        frame.add(vbox)
-        self.sidePage.add_widget(frame)
+        table = Gtk.Table(3, 2)
+        table.set_row_spacings(8)
+        table.set_col_spacings(15)        
+        self.sidePage.add_widget(table, False)
+        
+        label = Gtk.Label()
+        label.set_markup("<b>%s</b>" % _("Activation"))
+        label.set_alignment(1, 0.5)
+        table.attach(label, 0, 1, 0, 1)        
+                   
+
+        hbox = Gtk.HBox()
+
+        widget = GSettingsCheckButton(_("Lock the session when the screen goes to sleep"), "org.cinnamon.desktop.screensaver", "lock-enabled", None)
+        widget.set_tooltip_text(_("Enable this option to require a password when waking up the screen"))
+        hbox.pack_start (widget, False, False, 0)
+        widget = GSettingsIntComboBox(_(""), "org.cinnamon.desktop.screensaver", "lock-delay", "org.cinnamon.desktop.screensaver/lock-enabled", LOCK_DELAY_OPTIONS, use_uint=True)
+        widget.set_tooltip_text(_("This option defines the amount of time to wait before locking the screen, after the screen turns itself off"))
+        hbox.pack_start (widget, False, False, 6) 
+        table.attach(hbox, 1, 2, 1, 2, xoptions=Gtk.AttachOptions.EXPAND|Gtk.AttachOptions.FILL)        
+        
+        widget = GSettingsCheckButton(_("Lock the session when the computer suspends"), "org.cinnamon.settings-daemon.plugins.power", "lock-on-suspend", None)
+        widget.set_tooltip_text(_("Enable this option to require a password when the computer wakes up from suspend"))
+        table.attach(widget, 1, 2, 3, 4, xoptions=Gtk.AttachOptions.EXPAND|Gtk.AttachOptions.FILL)
+               
+        label = Gtk.Label()
+        label.set_markup("<b>%s</b>" % _("Away Message"))
+        label.set_alignment(1, 0.5)
+        table.attach(label, 0, 1, 4, 5)
+
+        widget = GSettingsEntry(_("Show this message when the screen is locked :"), "org.cinnamon.screensaver", "default-message", None)
+        widget.set_tooltip_text(_("This is the default message displayed on your lock screen"))
+        table.attach(widget, 1, 2, 5, 6, xoptions=Gtk.AttachOptions.EXPAND|Gtk.AttachOptions.FILL)
+        
+        widget = GSettingsCheckButton(_("Ask for a custom message when locking the screen from the menu"), "org.cinnamon.screensaver", "ask-for-away-message", None)
+        widget.set_tooltip_text(_("This option allows you to type a message each time you lock the screen from the menu"))        
+        table.attach(widget, 1, 2, 6, 7, xoptions=Gtk.AttachOptions.EXPAND|Gtk.AttachOptions.FILL)
+
+        
+
+        
