087797f6ff6fd52b08af8075b48f71ebe52ee57c
Michael Webster <miketwebster@gmail.com>
2014-04-01 22:45:30 -0400

    Disable xinput2 only for Cinnamon, not session-wide (moved from cinnamon-session).  Programs that use Clutter-Gtk need to be able to use it.
    Fixes #2993
    Fixes #2307
    Fixes #2830

--- src/main.c
+++ src/main.c
@@ -224,7 +224,7 @@ main (int argc, char **argv)
   GOptionContext *ctx;
   GError *error = NULL;
   int ecode;
-
+  g_setenv ("CLUTTER_DISABLE_XINPUT", "1", TRUE);
   g_type_init ();
 
   bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
@@ -251,6 +251,7 @@ main (int argc, char **argv)
   meta_init ();
   g_unsetenv ("NO_GAIL");
   g_unsetenv ("NO_AT_BRIDGE");
+  g_unsetenv ("CLUTTER_DISABLE_XINPUT");
 
   /* FIXME: Add gjs API to set this stuff and don't depend on the
    * environment.  These propagate to child processes.
