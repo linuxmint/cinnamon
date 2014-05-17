86bff1e8781d339b1ebc07b09da11bc6d6c3d7fd
Hamish Walker <stealthofking@googlemail.com>
2014-05-17 14:51:00 +0100

    Added notification settings module.

---
+++ files/usr/lib/cinnamon-settings/modules/cs_notifications.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from SettingsWidgets import *
+
+class Module:
+    def __init__(self, content_box):
+        keywords = _("notifications")
+        advanced = True
+        sidePage = SidePage(_("Notifications"), "general.svg", keywords, advanced, content_box)
+        self.sidePage = sidePage
+        self.name = "notifications"
+        self.category = "prefs"
+        sidePage.add_widget(GSettingsCheckButton(_("Display notifications"), "org.cinnamon", "display-notifications", None))
+
