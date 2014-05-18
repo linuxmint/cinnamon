9353671d4b6e044c17f5f75599c219d418392459
Hamish Walker <stealthofking@googlemail.com>
2014-05-18 03:19:53 +0100

    Updated Notification settings for 2.2.

--- files/usr/lib/cinnamon-settings/modules/cs_notifications.py
+++ files/usr/lib/cinnamon-settings/modules/cs_notifications.py
@@ -6,10 +6,16 @@ class Module:
     def __init__(self, content_box):
         keywords = _("notifications")
         advanced = True
-        sidePage = SidePage(_("Notifications"), "general.svg", keywords, advanced, content_box)
+        sidePage = SidePage(_("Notifications"), "se_general", keywords, advanced, content_box)
         self.sidePage = sidePage
         self.name = "notifications"
         self.category = "prefs"
+
+    def on_module_selected(self):
+        if self.loaded:
+            return
+        print "Loading notifications module"
+
         sidePage.add_widget(GSettingsCheckButton(_("Display notifications"), "org.cinnamon", "display-notifications", None))
         sidePage.add_widget(GSettingsCheckButton(_("Notifications fade out"), "org.cinnamon.desktop.notifications", "fade-on-mouseover", None))
         sidePage.add_widget(GSettingsCheckButton(_("Remove old notifications"), "org.cinnamon.desktop.notifications", "remove-old", None))
