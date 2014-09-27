b85d62eac609e42a163a824a28f8dbf0a5f6f14e
JosephMcc <mccullarjoseph@gmail.com>
2014-09-27 02:44:36 -0700

    add some border width to the add/remove window for choosing cinnamon themes so it matches the main window a bit more

--- files/usr/lib/cinnamon-settings/modules/cs_themes.py
+++ files/usr/lib/cinnamon-settings/modules/cs_themes.py
@@ -149,6 +149,7 @@ class Module:
         window.add(box)
         window.set_title(_("Desktop themes"))
         window.set_default_size(640, 480)
+        window.set_border_width(6)
         window.set_position(Gtk.WindowPosition.CENTER)        
         page = ExtensionSidePage(self.name, self.icon, self.keywords, box, "theme", "Cinnamon", None)
         page.builder = self.builder
