f9906169d46e264306facdb042d210fa3dfc12e9
dalcde <dalcde@yahoo.com.hk>
2015-01-11 20:10:38 +0000

    Use submenu for panel edit options

--- js/ui/panel.js
+++ js/ui/panel.js
@@ -936,10 +936,48 @@ function populateSettingsMenu(menu, panelId) {
     });
 
     menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-    
+
     menu.addMenuItem(menu.troubleshootItem);
 
     menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+    panelSettingsSection = new PopupMenu.PopupSubMenuMenuItem(_("Modify panel"), true);
+
+    let menuItem = new IconMenuItem(_("Remove panel"), "list-remove");
+    menuItem.activate = Lang.bind(this, function() {
+        Main.panelManager.removePanel(panelId);
+    });
+    panelSettingsSection.menu.addMenuItem(menuItem);
+
+    let menuItem = new IconMenuItem(_("Add panel"), "list-add");
+    menuItem.activate = Lang.bind(this, function() {
+        Main.panelManager.addPanelQuery();
+        this.close();
+    });
+    panelSettingsSection.menu.addMenuItem(menuItem);
+
+    let menuItem = new IconMenuItem(_("Move panel"), "move");
+    menuItem.activate = Lang.bind(menu, function() {
+        Main.panelManager.movePanelQuery(this.panelId);
+        this.close();
+    });
+    panelSettingsSection.menu.addMenuItem(menuItem);
+
+    let menuItem = new IconMenuItem(_("Copy applet configuration"), "edit-copy");
+    menuItem.activate = Lang.bind(menu, function() {
+        AppletManager.copyAppletConfiguration(this.panelId);
+        this.close();
+    });
+    panelSettingsSection.menu.addMenuItem(menuItem);
+
+    let menuItem = new IconMenuItem(_("Paste applet configuration"), "edit-paste");
+    menuItem.activate = Lang.bind(menu, function() {
+        let dialog = new AppletManager.ConfirmPasteDialog(this.panelId);
+        dialog.open();
+    });
+    panelSettingsSection.menu.addMenuItem(menuItem);
+
+    menu.addMenuItem(panelSettingsSection);
 
     // Auto-hide Panel
     let values = global.settings.get_strv(PANEL_AUTOHIDE_KEY);
@@ -1005,7 +1043,6 @@ PanelContextMenu.prototype = {
         Main.uiGroup.add_actor(this.actor);
         this.actor.hide();
         this.panelId = panelId;
-        global.settings.connect('changed::panel-edit-mode', Lang.bind(this, this._onPanelEditModeChanged));
 
         let applet_settings_item = new SettingsLauncher(_("Add applets to the panel"), "applets panel" + panelId, "list-add", this);
         this.addMenuItem(applet_settings_item);
@@ -1019,54 +1056,7 @@ PanelContextMenu.prototype = {
         let menuSetting = new SettingsLauncher(_("All settings"), "", "preferences-system", this);
         this.addMenuItem(menuSetting);
 
-        this.panelSettingsSection = new PopupMenu.PopupMenuSection();
-
-        let menuItem = new IconMenuItem(_("Remove panel"), "list-remove");
-        menuItem.activate = Lang.bind(this, function() {
-            Main.panelManager.removePanel(this.panelId);
-        });
-        this.panelSettingsSection.addMenuItem(menuItem);
-
-        let menuItem = new IconMenuItem(_("Add panel"), "list-add");
-        menuItem.activate = Lang.bind(this, function() {
-            Main.panelManager.addPanelQuery();
-            this.close();
-        });
-        this.panelSettingsSection.addMenuItem(menuItem);
-
-        let menuItem = new IconMenuItem(_("Move panel"), "move");
-        menuItem.activate = Lang.bind(this, function() {
-            Main.panelManager.movePanelQuery(this.panelId);
-            this.close();
-        });
-        this.panelSettingsSection.addMenuItem(menuItem);
-
-        let menuItem = new IconMenuItem(_("Copy applet configuration"), "edit-copy");
-        menuItem.activate = Lang.bind(this, function() {
-            AppletManager.copyAppletConfiguration(this.panelId);
-            this.close();
-        });
-        this.panelSettingsSection.addMenuItem(menuItem);
-
-        let menuItem = new IconMenuItem(_("Paste applet configuration"), "edit-paste");
-        menuItem.activate = Lang.bind(this, function() {
-            let dialog = new AppletManager.ConfirmPasteDialog(this.panelId);
-            dialog.open();
-        });
-        this.panelSettingsSection.addMenuItem(menuItem);
-
-        this.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-        this.addMenuItem(this.panelSettingsSection);
-
         populateSettingsMenu(this, panelId);
-        this._onPanelEditModeChanged();
-    },
-
-    _onPanelEditModeChanged: function() {
-        if (global.settings.get_boolean('panel-edit-mode'))
-            this.panelSettingsSection.actor.show();
-        else
-            this.panelSettingsSection.actor.hide();
     }
 }
 
