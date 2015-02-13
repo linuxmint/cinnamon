cda0fae20a5f748b1150553121f5f051997621b0
RavetcoFX <ravetcoFX@gmail.com>
2015-02-13 11:39:41 -0700

    Don't close menu every time a favorite is removed

--- files/usr/share/cinnamon/applets/menu@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/menu@cinnamon.org/applet.js
@@ -932,7 +932,6 @@ CategoriesApplicationsBox.prototype = {
     acceptDrop : function(source, actor, x, y, time) {
         if (source instanceof FavoritesButton){
             source.actor.destroy();
-            actor.destroy();
             AppFavorites.getAppFavorites().removeFavorite(source.app.get_id());
             return true;
         }
