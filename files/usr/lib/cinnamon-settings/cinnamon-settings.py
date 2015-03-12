06525636ba30b868e52b5e721c7ee75fcf9df592
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2015-03-12 12:58:55 +0100

    Cinnamon Settings: Added support for Blueberry

--- files/usr/lib/cinnamon-settings/cinnamon-settings.py
+++ files/usr/lib/cinnamon-settings/cinnamon-settings.py
@@ -80,7 +80,8 @@ STANDALONE_MODULES = [
     [_("Startup Applications"),          "cinnamon-session-properties",  "cs-startup-programs","prefs",          _("startup, programs, boot, init, session")],
     [_("Driver Manager"),                "mintdrivers",                  "cs-drivers",         "admin",          _("video, driver, wifi, card, hardware, proprietary, nvidia, radeon, nouveau, fglrx")],
     [_("Software Sources"),              "mintsources",                  "cs-sources",         "admin",          _("ppa, repository, package, source, download")],
-    [_("Users and Groups"),              "cinnamon-settings-users",      "cs-user-accounts",   "admin",          _("user, users, account, accounts, group, groups, password")]
+    [_("Users and Groups"),              "cinnamon-settings-users",      "cs-user-accounts",   "admin",          _("user, users, account, accounts, group, groups, password")],
+    [_("Bluetooth"),                     "blueberry",                    "cs-bluetooth",       "hardware",       _("bluetooth, dongle, transfer, mobile")]
 ]
 
 def print_timing(func):
