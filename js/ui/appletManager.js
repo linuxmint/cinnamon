67fbca65f923e78606d109a5cc0270e50bf5abd2
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2015-10-14 14:13:29 +0100

    Fixed Cinnamon caused by previous commits

--- js/ui/appletManager.js
+++ js/ui/appletManager.js
@@ -539,6 +539,7 @@ function pasteAppletConfiguration(panelId) {
                 if (curr[i].panelId == panelId) count --;
             }
         }
+    }
 
     clipboard.forEach(function(x) {
         let uuid = x.uuid
