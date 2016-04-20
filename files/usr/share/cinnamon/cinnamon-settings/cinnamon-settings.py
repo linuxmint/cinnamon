478751e07d57a39ca89d9732772e9f829c3959d0
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2016-04-20 12:05:49 +0100

    Cinnamon Settings: Retire cinnamon-bluetooth (replaced with Blueberry)

--- files/usr/share/cinnamon/cinnamon-settings/cinnamon-settings.py
+++ files/usr/share/cinnamon/cinnamon-settings/cinnamon-settings.py
@@ -66,7 +66,6 @@ CONTROL_CENTER_MODULES = [
 #         Label                              Module ID                Icon                         Category      Keywords for filter
     [_("Networking"),                       "network",            "cs-network",                 "hardware",      _("network, wireless, wifi, ethernet, broadband, internet")],
     [_("Display"),                          "display",            "cs-display",                 "hardware",      _("display, screen, monitor, layout, resolution, dual, lcd")],
-    [_("Bluetooth"),                        "bluetooth",          "cs-bluetooth",               "hardware",      _("bluetooth, dongle, transfer, mobile")],
     [_("Color"),                            "color",              "cs-color",                   "hardware",      _("color, profile, display, printer, output")],
     [_("Graphics Tablet"),                  "wacom",              "cs-tablet",                  "hardware",      _("wacom, digitize, tablet, graphics, calibrate, stylus")]
 ]
