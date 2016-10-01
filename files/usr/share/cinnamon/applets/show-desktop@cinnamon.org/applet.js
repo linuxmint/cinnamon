57cadff0b2a30ac92b1e8e5b4bf89dfd0c75a53a
itzexor <itzexor@gmail.com>
2016-09-30 18:19:26 -0700

    show desktop applet: various cleanups
    -Remove a bunch of extra whitespace
    -Rename _onEntered and _onLeaved to remove camel case
    -Break up lines longer than 120 characters
    -Various other minor cleanups

--- files/usr/share/cinnamon/applets/show-desktop@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/show-desktop@cinnamon.org/applet.js
@@ -15,86 +15,85 @@ MyApplet.prototype = {
 
     _init: function(orientation, panel_height, instance_id) {
         Applet.IconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
-        
-        /* Initialize your settings handler instance      this,            the uuid              instance id  */
+
         this.settings = new Settings.AppletSettings(this, "show-desktop@cinnamon.org", instance_id);
         
         this.settings.bindProperty(Settings.BindingDirection.IN, "peek-at-desktop", "peek_at_desktop", null, null);
         this.settings.bindProperty(Settings.BindingDirection.IN, "peek-delay", "peek_delay", null, null);
         this.settings.bindProperty(Settings.BindingDirection.IN, "peek-opacity", "peek_opacity", null, null);
         this.settings.bindProperty(Settings.BindingDirection.IN, "peek-blur", "peek_blur", null, null);
-                                  
-        this.actor.connect('enter-event', Lang.bind(this, this._onEntered));
-        this.actor.connect('leave-event', Lang.bind(this, this._onLeaved));
-        
-        this.didpeek=false;
+
+        this.actor.connect('enter-event', Lang.bind(this, this._on_enter));
+        this.actor.connect('leave-event', Lang.bind(this, this._on_leave));
+
+        this._did_peek = false;
         this._peek_timeout_id = 0;
-        
+
         this.set_applet_icon_name("user-desktop");
         this.set_applet_tooltip(_("Show desktop"));
     },
-    
+
     show_all_windows: function(time) {
         let windows = global.get_window_actors();
-        for(let i=0; i<windows.length; i++){
+        for(let i = 0; i < windows.length; i++){
             let window = windows[i].meta_window;
             let compositor = windows[i];
-            if(window.get_title()=="Desktop"){
+            if(window.get_title() == "Desktop"){
                 Tweener.addTween(compositor, { opacity: 255, time: time, transition: "easeOutSine" });
             }       
-            if (this.peek_blur && compositor.eff){
+            if (this.peek_blur && compositor.eff) {
                 compositor.remove_effect(compositor.eff);
             }
         }
-        Tweener.addTween(global.window_group, {  opacity: 255, time: time, transition: "easeOutSine" });
+        Tweener.addTween(global.window_group, { opacity: 255, time: time, transition: "easeOutSine" });
     },
-    
-    _onEntered: function(event) {
-        if (this.peek_at_desktop){  
-        
+
+    _on_enter: function(event) {
+        if (this.peek_at_desktop) {
+
             if (this._peek_timeout_id > 0) {
                 Mainloop.source_remove(this._peek_timeout_id);
                 this._peek_timeout_id = 0;
             }
 
-            this._peek_timeout_id = Mainloop.timeout_add(this.peek_delay, Lang.bind(this,function () {
-            
-                if(this.actor.hover && !this._applet_context_menu.isOpen && ! global.settings.get_boolean("panel-edit-mode")){
-                    Tweener.addTween(global.window_group, {opacity: this.peek_opacity, time: 0.275, transition: "easeInSine" });
-                    
+            this._peek_timeout_id = Mainloop.timeout_add(this.peek_delay, Lang.bind(this, function() {
+                if (this.actor.hover
+                    && !this._applet_context_menu.isOpen
+                    && !global.settings.get_boolean("panel-edit-mode")) {
+
+                    Tweener.addTween(global.window_group,
+                                     {opacity: this.peek_opacity, time: 0.275, transition: "easeInSine"});
+
                     let windows = global.get_window_actors();
-                    for(let i=0; i<windows.length; i++){
+                    for (let i = 0; i < windows.length; i++) {
                         let window = windows[i].meta_window;
                         let compositor = windows[i];
-                            
-                        if (this.peek_blur){
-                            if (!compositor.eff) 
+
+                        if (this.peek_blur) {
+                            if (!compositor.eff)
                                 compositor.eff = new Clutter.BlurEffect();
-                            compositor.add_effect_with_name('peek-blur',compositor.eff);
+                            compositor.add_effect_with_name('peek-blur', compositor.eff);
                         }
                     }
-                
-                    this.didpeek=true;
+                    this._did_peek = true;
                 }
-
                 this._peek_timeout_id = 0;
                 return false;
             }));
-            
         }
     },
-    
-    _onLeaved: function(event) {
-        if(this.didpeek){
+
+    _on_leave: function(event) {
+        if (this._did_peek) {
             this.show_all_windows(0.2);
-            this.didpeek=false;
+            this._did_peek = false;
         }
         if (this._peek_timeout_id > 0) {
             Mainloop.source_remove(this._peek_timeout_id);
             this._peek_timeout_id = 0;
         }
     },
-    
+
     on_applet_clicked: function(event) {
         global.screen.toggle_desktop(global.get_current_time());
         this.show_all_windows(0);
@@ -102,7 +101,7 @@ MyApplet.prototype = {
             Mainloop.source_remove(this._peek_timeout_id);
             this._peek_timeout_id = 0;
         }
-        this.didpeek=false;
+        this._did_peek = false;
     }
 };
 
