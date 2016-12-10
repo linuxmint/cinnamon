c62b3d62882454c60b8c43d0b34f0e98dce4915b
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2016-12-10 11:28:49 +0000

    Window list applet: Move preferences up in the context menu
    The close action is by far the most often used, and it's always been
    the closest one to the panel.
    
    This commit moves Preferences out of the way.
    
    It's less consistent across other applets, but it makes more sense
    for window-list.

--- files/usr/share/cinnamon/applets/window-list@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/window-list@cinnamon.org/applet.js
@@ -820,6 +820,24 @@ AppMenuButtonRightClickMenu.prototype = {
             }
         }
 
+        // Preferences
+        let subMenu = new PopupMenu.PopupSubMenuMenuItem(_("Preferences"));
+        this.addMenuItem(subMenu);
+
+        item = new PopupMenu.PopupIconMenuItem(_("About..."), "dialog-question", St.IconType.SYMBOLIC);
+        item.connect('activate', Lang.bind(this._launcher._applet, this._launcher._applet.openAbout));
+        subMenu.menu.addMenuItem(item);
+
+        item = new PopupMenu.PopupIconMenuItem(_("Configure..."), "system-run", St.IconType.SYMBOLIC);
+        item.connect('activate', Lang.bind(this._launcher._applet, this._launcher._applet.configureApplet));
+        subMenu.menu.addMenuItem(item);
+
+        item = new PopupMenu.PopupIconMenuItem(_("Remove 'Window list'"), "edit-delete", St.IconType.SYMBOLIC);
+        item.connect('activate', Lang.bind(this, function() {
+            AppletManager._removeAppletFromPanel(this._launcher._applet._uuid, this._launcher._applet.instance_id);
+        }));
+        subMenu.menu.addMenuItem(item);
+
         this.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
         // Close all/others
@@ -884,25 +902,6 @@ AppMenuButtonRightClickMenu.prototype = {
             mw.delete(global.get_current_time());
         });
         this.addMenuItem(item);
-
-        this.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-
-        let subMenu = new PopupMenu.PopupSubMenuMenuItem(_("Preferences"));
-        this.addMenuItem(subMenu);
-
-        item = new PopupMenu.PopupIconMenuItem(_("About..."), "dialog-question", St.IconType.SYMBOLIC);
-        item.connect('activate', Lang.bind(this._launcher._applet, this._launcher._applet.openAbout));
-        subMenu.menu.addMenuItem(item);
-
-        item = new PopupMenu.PopupIconMenuItem(_("Configure..."), "system-run", St.IconType.SYMBOLIC);
-        item.connect('activate', Lang.bind(this._launcher._applet, this._launcher._applet.configureApplet));
-        subMenu.menu.addMenuItem(item);
-
-        item = new PopupMenu.PopupIconMenuItem(_("Remove 'Window list'"), "edit-delete", St.IconType.SYMBOLIC);
-        item.connect('activate', Lang.bind(this, function() {
-            AppletManager._removeAppletFromPanel(this._launcher._applet._uuid, this._launcher._applet.instance_id);
-        }));
-        subMenu.menu.addMenuItem(item);
     },
 
     _onToggled: function(actor, isOpening){
