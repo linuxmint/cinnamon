2b8f3e836da9edeaa0ec86450ee4459dad1e2a93
brownsr <simonrossbrown@gmail.com>
2017-04-18 08:08:24 +0100

    st-widget: use g_clear_pointer https://github.com/GNOME/gnome-shell/commit/4d1358b7ed483b3822ab889e4851cca2c3fa8841#diff-9ad11fdf581414b0f2501e50921d1052

--- src/st/st-widget.c
+++ src/st/st-widget.c
@@ -287,25 +287,12 @@ st_widget_dispose (GObject *gobject)
   StWidget *actor = ST_WIDGET (gobject);
   StWidgetPrivate *priv = ST_WIDGET (actor)->priv;
 
-  if (priv->theme)
-    {
-      g_object_unref (priv->theme);
-      priv->theme = NULL;
-    }
-
-  if (priv->theme_node)
-    {
-      g_object_unref (priv->theme_node);
-      priv->theme_node = NULL;
-    }
+  g_clear_pointer (&priv->theme, g_object_unref);
+  g_clear_pointer (&priv->theme_node, g_object_unref);
 
   st_widget_remove_transition (actor);
 
-  if (priv->label_actor)
-    {
-      g_object_unref (priv->label_actor);
-      priv->label_actor = NULL;
-    }
+  g_clear_pointer (&priv->label_actor, g_object_unref);
 
   g_clear_object (&priv->prev_first_child);
   g_clear_object (&priv->prev_last_child);
