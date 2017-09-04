a039f7db996d7e049228a52eecba7513b1f4a2db
Jason Hicks <jaszhix@gmail.com>
2017-09-04 09:06:15 -0500

    overrides.js: Add Tweener patch from CJS
    Based on
    https://github.com/linuxmint/cjs/commit/ed7abaa82f0efded5ca155b81d0da47fef4cf956

--- js/ui/overrides.js
+++ js/ui/overrides.js
@@ -1,16 +1,18 @@
 // -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
 
 const Gio = imports.gi.Gio
-const Lang = imports.lang;
 const Mainloop = imports.mainloop;
 const GLib = imports.gi.GLib;
 const GObject = imports.gi.GObject;
+const Tweener = imports.tweener.tweener;
+const TweenList = imports.tweener.tweenList;
 
 function init() {
     overrideGio();
     overrideGObject();
     overrideMainloop();
     overrideJS();
+    overrideTweener();
 }
 
 function check_schema_and_init(obj, method, params) {
@@ -179,4 +181,283 @@ function overrideJS() {
         }
     };
     Object.defineProperty(Object.prototype, "maybeGet", {enumerable: false});
+}
+
+function overrideTweener() {
+    if (Tweener.restrictedWords.min != null) {
+        return;
+    }
+
+    Object.assign(Tweener.restrictedWords, {
+        min: true,
+        max: true
+    });
+
+    const originalTweenListClone = TweenList.TweenList.prototype.clone;
+    TweenList.TweenList.prototype.clone = function(omitEvents) {
+        const tween = originalTweenListClone(omitEvents);
+        tween.min = this.min;
+        tween.max = this.max;
+    };
+
+    Tweener._updateTweenByIndex = function(i) {
+        var tweening = Tweener._tweenList[i];
+
+        if (tweening == null || !tweening.scope)
+            return false;
+
+        var currentTime = Tweener._getCurrentTweeningTime(tweening);
+
+        if (currentTime < tweening.timeStart)
+            return true; // Hasn't started, so return true
+
+        var scope = tweening.scope;
+        var t, b, c, d, nv;
+
+        var isOver = false;
+
+        if (tweening.isCaller) {
+            do {
+                t = ((tweening.timeComplete - tweening.timeStart)/tweening.count) *
+                    (tweening.timesCalled + 1);
+                b = tweening.timeStart;
+                c = tweening.timeComplete - tweening.timeStart;
+                d = tweening.timeComplete - tweening.timeStart;
+                nv = tweening.transition(t, b, c, d);
+
+                if (currentTime >= nv) {
+                    Tweener._callOnFunction(tweening.onUpdate, "onUpdate", tweening.onUpdateScope,
+                                    scope, tweening.onUpdateParams);
+
+                    tweening.timesCalled++;
+                    if (tweening.timesCalled >= tweening.count) {
+                        isOver = true;
+                        break;
+                    }
+
+                    if (tweening.waitFrames)
+                        break;
+                }
+            } while (currentTime >= nv);
+        } else {
+            var mustUpdate, name;
+
+            if (currentTime >= tweening.timeComplete) {
+                isOver = true;
+                mustUpdate = true;
+            } else {
+                mustUpdate = tweening.skipUpdates < 1 ||
+                    !tweening.skipUpdates ||
+                    tweening.updatesSkipped >= tweening.skipUpdates;
+            }
+
+            if (!tweening.hasStarted) {
+                Tweener._callOnFunction(tweening.onStart, "onStart", tweening.onStartScope,
+                                scope, tweening.onStartParams);
+
+                for (name in tweening.properties) {
+                    var pv;
+
+                    if (tweening.properties[name].isSpecialProperty) {
+                        // It's a special property, tunnel via the special property function
+                        if (_specialPropertyList[name].preProcess != undefined) {
+                            tweening.properties[name].valueComplete = Tweener._specialPropertyList[name].preProcess(scope, Tweener._specialPropertyList[name].parameters, tweening.properties[name].originalValueComplete, tweening.properties[name].extra);
+                        }
+                        pv = Tweener._specialPropertyList[name].getValue(scope, Tweener._specialPropertyList[name].parameters, tweening.properties[name].extra);
+                    } else {
+                        // Directly read property
+                        pv = scope[name];
+                    }
+                    tweening.properties[name].valueStart = isNaN(pv) ? tweening.properties[name].valueComplete : pv;
+                }
+
+                mustUpdate = true;
+                tweening.hasStarted = true;
+            }
+
+            if (mustUpdate) {
+                for (name in tweening.properties) {
+                    var property = tweening.properties[name];
+
+                    if (isOver) {
+                        // Tweening time has finished, just set it to the final value
+                        nv = property.valueComplete;
+                    } else {
+                        if (property.hasModifier) {
+                            // Modified
+                            t = currentTime - tweening.timeStart;
+                            d = tweening.timeComplete - tweening.timeStart;
+                            nv = tweening.transition(t, 0, 1, d, tweening.transitionParams);
+                            nv = property.modifierFunction(property.valueStart, property.valueComplete, nv, property.modifierParameters);
+                        } else {
+                            // Normal update
+                            t = currentTime - tweening.timeStart;
+                            b = property.valueStart;
+                            c = property.valueComplete - property.valueStart;
+                            d = tweening.timeComplete - tweening.timeStart;
+                            nv = tweening.transition(t, b, c, d, tweening.transitionParams);
+                        }
+                    }
+
+                    if (tweening.rounded)
+                        nv = Math.round(nv);
+
+                    if (tweening.min !== undefined && nv < tweening.min)
+                        nv = tweening.min;
+                    if (tweening.max !== undefined && nv > tweening.max)
+                        nv = tweening.max;
+
+                    if (property.isSpecialProperty) {
+                        // It's a special property, tunnel via the special property method
+                        Tweener._specialPropertyList[name].setValue(scope, nv, _specialPropertyList[name].parameters, tweening.properties[name].extra);
+                    } else {
+                        // Directly set property
+                        scope[name] = nv;
+                    }
+                }
+
+                tweening.updatesSkipped = 0;
+
+                Tweener._callOnFunction(tweening.onUpdate, "onUpdate", tweening.onUpdateScope,
+                                scope, tweening.onUpdateParams);
+
+            } else {
+                tweening.updatesSkipped++;
+            }
+        }
+
+        if (isOver) {
+            Tweener._callOnFunction(tweening.onComplete, "onComplete", tweening.onCompleteScope,
+                            scope, tweening.onCompleteParams);
+        }
+
+        return !isOver;
+    }
+
+    Tweener._addTweenOrCaller = function(target, tweeningParameters, isCaller) {
+        if (!target)
+            return false;
+
+        var scopes; // List of objects to tween
+        if (target instanceof Array) {
+            // The first argument is an array
+            scopes = target.concat(); // XXX: To copy the array I guess
+        } else {
+            // The first argument(s) is(are) object(s)
+            scopes = new Array(target);
+        }
+
+        var obj, istr;
+
+        if (isCaller) {
+            obj = tweeningParameters;
+        } else {
+            obj = TweenList.makePropertiesChain(tweeningParameters);
+
+            var properties = Tweener._constructPropertyList(obj);
+
+            // Verifies whether the properties exist or not, for warning messages
+            for (istr in properties) {
+                if (Tweener._specialPropertyList[istr] != undefined) {
+                    properties[istr].isSpecialProperty = true;
+                } else {
+                    for (var i = 0; i < scopes.length; i++) {
+                        if (scopes[i][istr] == undefined)
+                            log("The property " + istr + " doesn't seem to be a normal object property of " + scopes[i] + " or a registered special property");
+                    }
+                    properties[istr].isSpecialProperty = false;
+                }
+            }
+        }
+
+        // Creates the main engine if it isn't active
+        if (!Tweener._inited) Tweener._init();
+        if (!Tweener._engineExists) Tweener._startEngine();
+
+        // Creates a "safer", more strict tweening object
+        var time = obj.time || 0;
+        var delay = obj.delay || 0;
+
+        var transition;
+
+        // FIXME: Tweener allows you to use functions with an all lower-case name
+        if (typeof obj.transition == "string") {
+            transition = imports.tweener.equations[obj.transition];
+        } else {
+            transition = obj.transition;
+        }
+
+        if (!transition)
+            transition = imports.tweener.equations["easeOutExpo"];
+
+        var tween;
+
+        for (let i = 0; i < scopes.length; i++) {
+            if (!isCaller) {
+                // Make a copy of the properties
+                var copyProperties = new Object();
+                for (istr in properties) {
+                    copyProperties[istr] = new Tweener.PropertyInfo(properties[istr].valueStart,
+                                                            properties[istr].valueComplete,
+                                                            properties[istr].valueComplete,
+                                                            properties[istr].arrayIndex || 0,
+                                                            {},
+                                                            properties[istr].isSpecialProperty,
+                                                            properties[istr].modifierFunction || null,
+                                                            properties[istr].modifierParameters || null);
+                }
+            }
+
+            tween = new TweenList.TweenList(scopes[i],
+                                            Tweener._ticker.getTime() + ((delay * 1000) / Tweener._timeScale),
+                                            Tweener._ticker.getTime() + (((delay * 1000) + (time * 1000)) / Tweener._timeScale),
+                                            false,
+                                            transition,
+                                            obj.transitionParams || null);
+
+            tween.properties               =       isCaller ? null : copyProperties;
+            tween.onStart                  =       obj.onStart;
+            tween.onUpdate                 =       obj.onUpdate;
+            tween.onComplete               =       obj.onComplete;
+            tween.onOverwrite              =       obj.onOverwrite;
+            tween.onError                  =       obj.onError;
+            tween.onStartParams            =       obj.onStartParams;
+            tween.onUpdateParams           =       obj.onUpdateParams;
+            tween.onCompleteParams         =       obj.onCompleteParams;
+            tween.onOverwriteParams        =       obj.onOverwriteParams;
+            tween.onStartScope             =       obj.onStartScope;
+            tween.onUpdateScope            =       obj.onUpdateScope;
+            tween.onCompleteScope          =       obj.onCompleteScope;
+            tween.onOverwriteScope         =       obj.onOverwriteScope;
+            tween.onErrorScope             =       obj.onErrorScope;
+            tween.rounded                  =       obj.rounded;
+            tween.min                      =       obj.min;
+            tween.max                      =       obj.max;
+            tween.skipUpdates              =       obj.skipUpdates;
+            tween.isCaller                 =       isCaller;
+
+            if (isCaller) {
+                tween.count = obj.count;
+                tween.waitFrames = obj.waitFrames;
+            }
+
+            if (!isCaller) {
+                // Remove other tweenings that occur at the same time
+                Tweener.removeTweensByTime(tween.scope, tween.properties, tween.timeStart, tween.timeComplete);
+            }
+
+            // And finally adds it to the list
+            Tweener._tweenList.push(tween);
+
+            // Immediate update and removal if it's an immediate tween
+            // If not deleted, it executes at the end of this frame execution
+            if (time == 0 && delay == 0) {
+                var myT = Tweener._tweenList.length-1;
+                Tweener._updateTweenByIndex(myT);
+                Tweener._removeTweenByIndex(myT);
+            }
+        }
+
+        return true;
+    };
 }
