8d9b677544a0129063ec343f5ba06a0cf8de27ce
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2017-10-10 12:39:43 +0100

    On-screen keyboard: Don't register Onboard's DBUS name
    There's a much better fix to prevent Onboard from autostarting
    and thus interfering with AT-SPI events and our own on-screen
    keyboard:
    
    https://github.com/linuxmint/onboard/commit/d03e767431225530b6b889aad9eb5a56a98e74f3
    https://bugs.launchpad.net/onboard/+bug/1722511
    
    Fixes #6816

--- src/main.c
+++ src/main.c
@@ -148,10 +148,6 @@ cinnamon_dbus_init (gboolean replace)
                            DBUS_NAME_FLAG_REPLACE_EXISTING,
                            &request_name_result,
                            "org.gnome.Caribou.Keyboard", FALSE);
-  cinnamon_dbus_acquire_name (bus,
-                           DBUS_NAME_FLAG_REPLACE_EXISTING,
-                           &request_name_result,
-                           "org.onboard.Onboard", FALSE);
   g_object_unref (bus);
   g_object_unref (session);
 }
