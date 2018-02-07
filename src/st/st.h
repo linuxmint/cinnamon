b8d78d85d9d37e3b2d899d565188870b21bd1621
Félix Piédallu <felix@piedallu.me>
2018-02-07 11:17:19 +0100

    Add meson files

---
+++ src/st/st.h
@@ -0,0 +1,37 @@
+#pragma once
+#define ST_H_INSIDE 1
+
+#include <st-adjustment.h>
+#include <st-background-effect.h>
+#include <st-bin.h>
+#include <st-border-image.h>
+#include <st-box-layout-child.h>
+#include <st-box-layout.h>
+#include <st-button.h>
+#include <st-clipboard.h>
+#include <st-cogl-wrapper.h>
+#include <st-drawing-area.h>
+#include <st-entry.h>
+#include <st-focus-manager.h>
+#include <st-group.h>
+#include <st-icon-colors.h>
+#include <st-icon.h>
+#include <st-im-text.h>
+#include <st-label.h>
+#include <st-polygon.h>
+#include <st-private.h>
+#include <st-scroll-bar.h>
+#include <st-scroll-view.h>
+#include <st-scrollable.h>
+#include <st-shadow.h>
+#include <st-table-child.h>
+#include <st-table.h>
+#include <st-texture-cache.h>
+#include <st-theme-context.h>
+#include <st-theme-node.h>
+#include <st-theme.h>
+#include <st-types.h>
+#include <st-widget-accessible.h>
+#include <st-widget.h>
+
+#undef ST_H_INSIDE
