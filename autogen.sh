cb23406268d431a65027333a574ab52520d89407
Kevin Gallagher <kevingallagher@gmail.com>
2018-02-16 23:32:45 -0800

    autogen.sh: Fixes aclocal warnings from missing gettext macros
        Fixes the following error  on Debian 9: "Please add the files
        codeset.m4 gettext.m4 glibc21.m4 iconv.m4 isc-posix.m4 lcmessage.m4
        ogtest.m4 from the /usr/local/share/aclocal directory to your autoconf
        macro directory or directly to your aclocal.m4 file."
    
        Only works on second run.

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,10 @@
 # Run this to generate all the initial makefiles, etc.
 test -n "$srcdir" || srcdir=$(dirname "$0")
 test -n "$srcdir" || srcdir=.
+export srcdir
 
 olddir=$(pwd)
-
-cd $srcdir
+cd "$srcdir"
 
 (test -f configure.ac) || {
     echo "*** ERROR: Directory '$srcdir' does not look like the top-level project directory ***"
@@ -14,7 +14,6 @@ cd $srcdir
 
 # shellcheck disable=SC2016
 PKG_NAME=$(autoconf --trace 'AC_INIT:$1' configure.ac)
-
 if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
     echo "*** WARNING: I am going to run 'configure' with no arguments." >&2
     echo "*** If you wish to pass any to it, please specify them on the" >&2
@@ -23,6 +22,20 @@ if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
 fi
 
 mkdir -p m4
+ACLOCAL_DIR="$(aclocal --print-ac-dir)"
+set -a 'codeset.m4' 'gettext.m4' 'glibc21.m4' 'iconv.m4' 'lcmessage.m4' 'progtest.m4'
+NUM_MACROS="$#"; COUNT=1
+for i in "$@"; do
+    MACRO_PATH="$ACLOCAL_DIR/$i"
+    if [ -f "$MACRO_PATH" ]; then
+        (cp --no-preserve=mode,ownership "${MACRO_PATH}" "${srcdir}/m4/")2>/dev/null && (echo Copied $i into ${srcdir}/m4)
+    else
+        echo "$i could not be found in ${ACLOCAL_DIR}" >&2
+    fi
+
+    if [ "$COUNT" -ge "${NUM_MACROS}" ]; then echo "Done copying aclocal *.m4 files."; unset MACRO_PATH; break; fi
+    COUNT=$((COUNT + 1))
+done
 
 glib-gettextize --force --copy || exit 1
 gtkdocize --copy || exit 1
@@ -32,7 +45,6 @@ autoreconf --verbose --force --install || exit 1
 cd "$olddir"
 if [ "$NOCONFIGURE" = "" ]; then
     $srcdir/configure "$@" || exit 1
-
     if [ "$1" = "--help" ]; then exit 0 else
         echo "Now type 'make' to compile $PKG_NAME" || exit 1
     fi
