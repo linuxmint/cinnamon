d5a098b33952c4ca1663eecf72379bbbf3e08da2
Jason Hicks <jaszhix@gmail.com>
2018-04-14 21:52:53 -0500

    Remove unused get_children call

--- files/usr/share/cinnamon/applets/notifications@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/notifications@cinnamon.org/applet.js
@@ -247,9 +247,9 @@ MyApplet.prototype = {
     },
 
     _update_timestamp: function () {
-        let actors = this._notificationbin.get_children();
-        if (actors) {
-            for (let i = 0; i < this.notifications.length; i++) {
+        let len = this.notifications.length;
+        if (len > 0) {
+            for (let i = 0; i < len; i++) {
                 let notification = this.notifications[i];
                 let orig_time = notification._timestamp;
                 notification._timeLabel.clutter_text.set_markup(timeify(orig_time));
