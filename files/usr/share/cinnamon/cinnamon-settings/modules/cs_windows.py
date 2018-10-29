aa431c2ec79dde0508e4e767bcdbcbfa0456a207
Michael Webster <miketwebster@gmail.com>
2018-10-28 21:58:02 -0400

    cs_windows.py: Update edge resistance label and tooltip to reflect changed functionality.
    see:
    https://github.com/linuxmint/muffin/commit/93869af456e28a03baed0f5b8958dfcb0ae5a546

--- files/usr/share/cinnamon/cinnamon-settings/modules/cs_windows.py
+++ files/usr/share/cinnamon/cinnamon-settings/modules/cs_windows.py
@@ -103,8 +103,8 @@ class Module:
             widget = GSettingsSpinButton(_("Window drag/resize threshold"), "org.cinnamon.muffin", "resize-threshold", _("Pixels"), 1, 100, size_group=size_group)
             settings.add_row(widget)
 
-            widget = GSettingsSwitch(_("Edge resistance with other windows"), "org.cinnamon.muffin", "edge-resistance-window")
-            widget.set_tooltip_text(_("Make window borders stick when moved or resized near other windows."))
+            widget = GSettingsSwitch(_("Edge resistance with other windows and monitor boundaries"), "org.cinnamon.muffin", "edge-resistance-window")
+            widget.set_tooltip_text(_("Make window borders stick when moved or resized near other windows or monitor edges."))
             settings.add_row(widget)
 
             # Alt Tab
