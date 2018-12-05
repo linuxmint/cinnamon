9b88d8251d3a89def5c84953112d8414efba416f
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2018-12-05 12:35:54 +0000

    Settings: Fix mintlocale-im launcher
    "mintlocale im" is now "mintlocale-im" (seperate app) in Mint 19.1.

--- files/usr/share/cinnamon/cinnamon-settings/cinnamon-settings.py
+++ files/usr/share/cinnamon/cinnamon-settings/cinnamon-settings.py
@@ -79,7 +79,7 @@ STANDALONE_MODULES = [
     [_("Firewall"),                      "gufw",                                "cs-firewall",        "admin",          _("firewall, block, filter, programs")],
     [_("Firewall"),                      "firewall-config",                     "cs-firewall",        "admin",          _("firewall, block, filter, programs")],
     [_("Languages"),                     "mintlocale",                          "cs-language",        "prefs",          _("language, install, foreign")],
-    [_("Input Method"),                  "mintlocale im",                       "cs-input-method",    "prefs",          _("language, install, foreign, input, method, chinese, korean, japanese, typing")],
+    [_("Input Method"),                  "mintlocale-im",                       "cs-input-method",    "prefs",          _("language, install, foreign, input, method, chinese, korean, japanese, typing")],
     [_("Login Window"),                  "pkexec lightdm-settings",             "cs-login",           "admin",          _("login, lightdm, mdm, gdm, manager, user, password, startup, switch")],
     [_("Login Window"),                  "lightdm-gtk-greeter-settings-pkexec", "cs-login",           "admin",          _("login, lightdm, manager, settings, editor")],
     [_("Driver Manager"),                "pkexec driver-manager",               "cs-drivers",         "admin",          _("video, driver, wifi, card, hardware, proprietary, nvidia, radeon, nouveau, fglrx")],
