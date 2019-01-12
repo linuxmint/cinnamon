0e66bc0d60f2de9f22104fe445ae3d6958d04c1d
Jason Hicks <jaszhix@gmail.com>
2019-01-12 10:22:54 -0600

    clock desklet: Use async settings

--- files/usr/share/cinnamon/desklets/clock@cinnamon.org/desklet.js
+++ files/usr/share/cinnamon/desklets/clock@cinnamon.org/desklet.js
@@ -1,27 +1,33 @@
 
-const St = imports.gi.St;
-const CinnamonDesktop = imports.gi.CinnamonDesktop;
+const {Label} = imports.gi.St;
+const {WallClock} = imports.gi.CinnamonDesktop;
 
-const Desklet = imports.ui.desklet;
-const Settings = imports.ui.settings;
+const {Desklet} = imports.ui.desklet;
+const {DeskletSettings} = imports.ui.settings;
 
-class CinnamonClockDesklet extends Desklet.Desklet {
+class CinnamonClockDesklet extends Desklet {
     constructor(metadata, desklet_id) {
         super(metadata, desklet_id);
-        this._date = new St.Label({style_class: "clock-desklet-label"});
+        this._date = new Label({style_class: 'clock-desklet-label'});
         this.setContent(this._date);
         this.setHeader(_("Clock"));
 
-        this.clock = new CinnamonDesktop.WallClock();
+        this.clock = new WallClock();
         this.clock_notify_id = 0;
 
-        this.settings = new Settings.DeskletSettings(this, this.metadata["uuid"], desklet_id);
-        this.settings.bind("date-format", "format");
-        this.settings.bind("font-size", "size", this._onSettingsChanged);
-        this.settings.bind("text-color", "color", this._onSettingsChanged);
-        this.settings.bind("use-custom-format", "use_custom_format", this._onSettingsChanged);
+        this.state = {};
 
-        this._menu.addSettingsAction(_("Date and Time Settings"), "calendar")
+        this.settings = new DeskletSettings(this.state, metadata.uuid, desklet_id, true);
+        this.settings.promise.then(() => this.settingsInit());
+    }
+
+    settingsInit() {
+        this.settings.bind('date-format', 'format');
+        this.settings.bind('font-size', 'size', () => this._onSettingsChanged());
+        this.settings.bind('text-color', 'color', () => this._onSettingsChanged());
+        this.settings.bind('use-custom-format', 'use_custom_format', () => this._onSettingsChanged());
+
+        this._menu.addSettingsAction(_('Date and Time Settings'), 'calendar');
     }
 
     _clockNotify(obj, pspec, data) {
@@ -29,7 +35,7 @@ class CinnamonClockDesklet extends Desklet.Desklet {
     }
 
     _onSettingsChanged() {
-        this._date.style="font-size: " + this.size + "pt;\ncolor: " + this.color;
+        this._date.style = `font-size:${this.state.size}pt;color:${this.state.color}`;
         this._updateFormatString();
         this._updateClock();
     }
@@ -47,11 +53,12 @@ class CinnamonClockDesklet extends Desklet.Desklet {
             this.clock.disconnect(this.clock_notify_id);
             this.clock_notify_id = 0;
         }
+        this.settings.finalize();
     }
 
     _updateFormatString() {
-        if (this.use_custom_format) {
-            if (!this.clock.set_format_string(this.format)) {
+        if (this.state.use_custom_format) {
+            if (!this.clock.set_format_string(this.state.format)) {
                 global.logError("Clock desklet: bad format - check your string.");
                 this.clock.set_format_string("~FORMAT ERROR~ %l:%M %p");
             }
@@ -61,7 +68,7 @@ class CinnamonClockDesklet extends Desklet.Desklet {
     }
 
     _updateClock() {
-        if (this.use_custom_format) {
+        if (this.state.use_custom_format) {
             this._date.set_text(this.clock.get_clock());
         } else {
             let default_format = this.clock.get_default_time_format();
