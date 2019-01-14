f96b13bc500f0fc7eb4466cdad4b76bc882becd3
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2019-01-14 13:15:49 +0000

    ShellApp: Fix warning with call to g_strv_length()
    There is no implicit cast from const char * const * to char **.
    
    https://github.com/GNOME/gnome-shell/commit/46caf6d673f2227ab3df2d4393ba0125c6447104

--- src/cinnamon-app.c
+++ src/cinnamon-app.c
@@ -1476,7 +1476,7 @@ cinnamon_app_init_search_data (CinnamonApp *app)
     {
       int i;
 
-      app->casefolded_keywords = g_new0 (char*, g_strv_length (keywords) + 1);
+      app->casefolded_keywords = g_new0 (char*, g_strv_length ((char **)keywords) + 1);
 
       i = 0;
       while (keywords[i])
