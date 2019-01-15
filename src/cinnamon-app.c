ac1cbd1731da00a16a3c21fe38473cb3d7eafabd
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2019-01-15 15:51:42 +0000

    app: Add a construct-only app-info property
    This will allow us to create a ShellApp from JS code, for instance
    for .desktop files that aren't known to the AppSystem.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=762206
    https://github.com/GNOME/gnome-shell/commit/2f0039e7dd11e86ecd4c66ee9ca52d05fa3fe296

--- src/cinnamon-app.c
+++ src/cinnamon-app.c
@@ -97,7 +97,8 @@ enum {
   PROP_ID,
   PROP_DBUS_ID,
   PROP_ACTION_GROUP,
-  PROP_MENU
+  PROP_MENU,
+  PROP_APP_INFO
 };
 
 enum {
@@ -138,6 +139,29 @@ cinnamon_app_get_property (GObject    *gobject,
     case PROP_MENU:
       if (app->running_state)
         g_value_set_object (value, app->running_state->remote_menu);
+      break;
+    case PROP_APP_INFO:
+      if (app->info)
+        g_value_set_object (value, app->info);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (gobject, prop_id, pspec);
+      break;
+    }
+}
+
+static void
+cinnamon_app_set_property (GObject      *gobject,
+                        guint         prop_id,
+                        const GValue *value,
+                        GParamSpec   *pspec)
+{
+  CinnamonApp *app = CINNAMON_APP (gobject);
+
+  switch (prop_id)
+    {
+    case PROP_APP_INFO:
+      _cinnamon_app_set_app_info (app, g_value_get_object (value));
       break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (gobject, prop_id, pspec);
@@ -943,9 +967,9 @@ _cinnamon_app_new (GDesktopAppInfo *info)
 {
   CinnamonApp *app;
 
-  app = g_object_new (CINNAMON_TYPE_APP, NULL);
-
-  _cinnamon_app_set_app_info (app, info);
+  app = g_object_new (CINNAMON_TYPE_APP,
+                      "app-info", info,
+                      NULL);
 
   return app;
 }
@@ -954,12 +978,11 @@ void
 _cinnamon_app_set_app_info (CinnamonApp       *app,
                             GDesktopAppInfo   *info)
 {
-  g_clear_object (&app->info);
-  app->info = g_object_ref (info);
+  g_set_object (&app->info, info);
 
-  if (app->name_collation_key != NULL)
-    g_free (app->name_collation_key);
-  app->name_collation_key = g_utf8_collate_key (cinnamon_app_get_name (app), -1);
+  g_clear_pointer (&app->name_collation_key, g_free);
+  if (app->info)
+    app->name_collation_key = g_utf8_collate_key (cinnamon_app_get_name (app), -1);
 }
 
 static void
@@ -1549,6 +1572,7 @@ cinnamon_app_class_init(CinnamonAppClass *klass)
   GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
 
   gobject_class->get_property = cinnamon_app_get_property;
+  gobject_class->set_property = cinnamon_app_set_property;
   gobject_class->dispose = cinnamon_app_dispose;
   gobject_class->finalize = cinnamon_app_finalize;
 
@@ -1627,5 +1651,20 @@ cinnamon_app_class_init(CinnamonAppClass *klass)
                                                         "The primary menu exported by the remote application",
                                                         G_TYPE_MENU_MODEL,
                                                         G_PARAM_READABLE | G_PARAM_STATIC_STRINGS));
+
+    /**
+   * CinnamonApp:app-info:
+   *
+   * The #GDesktopAppInfo associated with this CinnamonApp, if any.
+   */
+  g_object_class_install_property (gobject_class,
+                                   PROP_APP_INFO,
+                                   g_param_spec_object ("app-info",
+                                                        "DesktopAppInfo",
+                                                        "The DesktopAppInfo associated with this app",
+                                                        G_TYPE_DESKTOP_APP_INFO,
+                                                        G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS));
+
+
 
 }
