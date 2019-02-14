4155fa6d10b5a42d6e5bed76c619af1f83487db1
Daniel Prosser <dpross1100@msn.com>
2019-02-13 23:00:12 -0500

    Allow suspend/hibernate/hybrid sleep via ConsoleKit in power management

--- files/usr/share/cinnamon/cinnamon-settings/modules/cs_power.py
+++ files/usr/share/cinnamon/cinnamon-settings/modules/cs_power.py
@@ -547,6 +547,7 @@ def get_available_options(up_client):
     can_hibernate = False
     can_hybrid_sleep = False
 
+    # Try logind first
     try:
         connection = Gio.bus_get_sync(Gio.BusType.SYSTEM, None)
         proxy = Gio.DBusProxy.new_sync(
@@ -561,6 +562,24 @@ def get_available_options(up_client):
         can_suspend = proxy.CanSuspend() == "yes"
         can_hibernate = proxy.CanHibernate() == "yes"
         can_hybrid_sleep = proxy.CanHybridSleep() == "yes"
+    except:
+        pass
+
+    # Next try ConsoleKit
+    try:
+        connection = Gio.bus_get_sync(Gio.BusType.SYSTEM, None)
+        proxy = Gio.DBusProxy.new_sync(
+            connection,
+            Gio.DBusProxyFlags.NONE,
+            None,
+            "org.freedesktop.ConsoleKit",
+            "/org/freedesktop/ConsoleKit/Manager",
+            "org.freedesktop.ConsoleKit.Manager",
+            None)
+
+        can_suspend = can_suspend or (proxy.CanSuspend() == "yes")
+        can_hibernate = can_hibernate or (proxy.CanHybridSleep() == "yes")
+        can_hybrid_sleep = can_hybrid_sleep or (proxy.CanHybridSleep() == "yes")
     except:
         pass
 
