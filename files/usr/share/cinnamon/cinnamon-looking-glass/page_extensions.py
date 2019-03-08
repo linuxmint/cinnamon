68c0dbde81320ad4ae071189ac7e15e264f0e5f7
James Ross <itzexor@gmail.com>
2019-03-08 13:16:39 -0800

    Melange/all: huge style refactor
    pylintrc:
    - add `unused-argument` and `wrong-import-posistion` to ignored warnings
      since we trigger these often with gi callback signatures and the way
      we have to specify library versions before import via a function call.
    - ignore some common size/width vars from naming restrictions
    - allow loading some common modules so that we get less undefined
      references
    
    melange:
    - convert everything except dbus methods to snake_case
    - various other style cleanups here and there
    - remove unused memory page, as this is no longer supported by cjs
      and I haven't seen any replacement functionality.

--- files/usr/share/cinnamon/cinnamon-looking-glass/page_extensions.py
+++ files/usr/share/cinnamon/cinnamon-looking-glass/page_extensions.py
@@ -1,56 +1,57 @@
 #!/usr/bin/python3
 
-import pageutils
 import os
 from gi.repository import Gtk, Gdk
+import pageutils
 
 class ModulePage(pageutils.BaseListView):
     def __init__(self, parent):
         store = Gtk.ListStore(str, str, str, str, str, str, str, bool, str)
         pageutils.BaseListView.__init__(self, store)
         self.parent = parent
+        self.selected_path = None
 
-        self.createTextColumn(0, "Status")
-        self.createTextColumn(1, "Type")
-        self.createTextColumn(2, "Name")
-        self.createTextColumn(3, "Description")
-        self.getUpdates()
-        lookingGlassProxy.connect("ExtensionListUpdate", self.getUpdates)
-        lookingGlassProxy.addStatusChangeCallback(self.onStatusChange)
-        self.treeView.set_tooltip_column(8)
+        self.create_text_column(0, "Status")
+        self.create_text_column(1, "Type")
+        self.create_text_column(2, "Name")
+        self.create_text_column(3, "Description")
+        self.get_updates()
+        parent.lg_proxy.connect("ExtensionListUpdate", self.get_updates)
+        parent.lg_proxy.add_status_change_callback(self.on_status_change)
+        self.tree_view.set_tooltip_column(8)
 
         self.popup = Gtk.Menu()
 
-        self.viewSource = Gtk.MenuItem('View Source')
-        self.viewSource.connect("activate", self.onViewSource)
-        self.popup.append(self.viewSource)
+        self.view_source = Gtk.MenuItem('View Source')
+        self.view_source.connect("activate", self.on_view_source)
+        self.popup.append(self.view_source)
 
-        reloadCode = Gtk.MenuItem('Reload Code')
-        reloadCode.connect("activate", self.onReloadCode)
-        self.popup.append(reloadCode)
+        reload_code = Gtk.MenuItem('Reload Code')
+        reload_code.connect("activate", self.on_reload_code)
+        self.popup.append(reload_code)
 
-        self.viewWebPage = Gtk.MenuItem('View Web Page')
-        self.viewWebPage.connect("activate", self.onViewWebPage)
-        self.popup.append(self.viewWebPage)
+        self.view_web_page = Gtk.MenuItem('View Web Page')
+        self.view_web_page.connect("activate", self.on_view_web_page)
+        self.popup.append(self.view_web_page)
 
         self.popup.show_all()
 
-        self.treeView.connect("button-press-event", self.on_button_press_event)
+        self.tree_view.connect("button-press-event", self.on_button_press_event)
 
-    def onViewSource(self, menuItem):
-        treeIter = self.store.get_iter(self.selectedPath)
-        folder = self.store.get_value(treeIter, 5)
+    def on_view_source(self, menu_item):
+        tree_iter = self.store.get_iter(self.selected_path)
+        folder = self.store.get_value(tree_iter, 5)
         os.system("xdg-open \"" + folder + "\" &")
 
-    def onReloadCode(self, menuItem):
-        treeIter = self.store.get_iter(self.selectedPath)
-        uuid = self.store.get_value(treeIter, 4)
-        xletType = self.store.get_value(treeIter, 1)
-        lookingGlassProxy.ReloadExtension(uuid, xletType.upper())
+    def on_reload_code(self, menu_item):
+        tree_iter = self.store.get_iter(self.selected_path)
+        uuid = self.store.get_value(tree_iter, 4)
+        xlet_type = self.store.get_value(tree_iter, 1)
+        self.parent.lg_proxy.ReloadExtension(uuid, xlet_type.upper())
 
-    def onViewWebPage(self, menuItem):
-        treeIter = self.store.get_iter(self.selectedPath)
-        url = self.store.get_value(treeIter, 6)
+    def on_view_web_page(self, menu_item):
+        tree_iter = self.store.get_iter(self.selected_path)
+        url = self.store.get_value(tree_iter, 6)
         os.system("xdg-open \"" + url + "\" &")
 
     def on_button_press_event(self, treeview, event):
@@ -59,34 +60,42 @@ class ModulePage(pageutils.BaseListView):
         pthinfo = treeview.get_path_at_pos(x, y)
         if pthinfo is not None:
             path, col, cellx, celly = pthinfo
-            self.selectedPath = path
+            self.selected_path = path
             treeview.grab_focus()
-            treeview.set_cursor( path, col, 0)
+            treeview.set_cursor(path, col, 0)
 
-            treeIter = self.store.get_iter(self.selectedPath)
+            tree_iter = self.store.get_iter(self.selected_path)
 
         if event.button == 3:
             if pthinfo is not None:
-                uuid = self.store.get_value(treeIter, 4)
-                url = self.store.get_value(treeIter, 6)
+                uuid = self.store.get_value(tree_iter, 4)
+                url = self.store.get_value(tree_iter, 6)
 
-                self.viewWebPage.set_sensitive(url != "")
-                self.viewSource.set_label(uuid + " (View Source)")
-                self.popup.popup( None, None, None, None, event.button, event.time)
+                self.view_web_page.set_sensitive(url != "")
+                self.view_source.set_label(uuid + " (View Source)")
+                self.popup.popup(None, None, None, None, event.button, event.time)
             return True
         elif event.type == Gdk.EventType.DOUBLE_BUTTON_PRESS:
             if pthinfo is not None:
-                error = self.store.get_value(treeIter, 7)
+                error = self.store.get_value(tree_iter, 7)
                 if error:
-                    self.parent.activatePage("log")
+                    self.parent.activate_page("log")
 
-    def onStatusChange(self, online):
+    def on_status_change(self, online):
         if online:
-            self.getUpdates()
+            self.get_updates()
 
-    def getUpdates(self):
-        success, data = lookingGlassProxy.GetExtensionList()
+    def get_updates(self):
+        success, data = self.parent.lg_proxy.GetExtensionList()
         if success:
             self.store.clear()
             for item in data:
-                self.store.append([item["status"], item["type"], item["name"], item["description"], item["uuid"], item["folder"], item["url"], item["error"] == "true", item["error_message"]])
+                self.store.append([item["status"],
+                                   item["type"],
+                                   item["name"],
+                                   item["description"],
+                                   item["uuid"],
+                                   item["folder"],
+                                   item["url"],
+                                   item["error"] == "true",
+                                   item["error_message"]])
--- files/usr/share/cinnamon/cinnamon-looking-glass/page_log.py
+++ files/usr/share/cinnamon/cinnamon-looking-glass/page_log.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 import datetime
-import pageutils
 from gi.repository import Gtk
+import pageutils
 
 class LogEntry():
     def __init__(self, category, time, message):
@@ -10,11 +10,12 @@ class LogEntry():
         self.time = int(time)
         self.timestr = datetime.datetime.fromtimestamp(self.time).strftime("%Y-%m-%dT%H:%M:%SZ")
         self.message = message
-        self.formattedText = "%s t=%s %s\n" % (category, self.timestr, message)
+        self.formatted_text = "%s t=%s %s\n" % (category, self.timestr, message)
 
 class LogView(Gtk.ScrolledWindow):
-    def __init__(self):
+    def __init__(self, proxy):
         Gtk.ScrolledWindow.__init__(self)
+        self.proxy = proxy
 
         self.set_shadow_type(Gtk.ShadowType.ETCHED_IN)
         self.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
@@ -25,88 +26,119 @@ class LogView(Gtk.ScrolledWindow):
         self.add(self.textview)
 
         self.textbuffer = self.textview.get_buffer()
-        self.scroll_mark =  self.textbuffer.create_mark(None, self.textbuffer.get_end_iter(), False)
+        self.scroll_mark = self.textbuffer.create_mark(None, self.textbuffer.get_end_iter(), False)
 
         self.log = []
-        self.addedMessages = 0
-        self.firstMessageTime = None
+        self.added_messages = 0
+        self.first_message_time = None
 
-        self.enabledTypes = {'info': True, 'warning': True, 'error': True, 'trace': False }
-        self.typeTags = {
-            'info': self.textbuffer.create_tag("info", foreground="#1a6f18", invisible=self.enabledTypes["info"] != True, invisible_set=True),
-            'warning': self.textbuffer.create_tag("warning", foreground="#c8bf33", invisible=self.enabledTypes["warning"] != True, invisible_set=True),
-            'error': self.textbuffer.create_tag("error", foreground="#9f1313", invisible=self.enabledTypes["error"] != True, invisible_set=True),
-            'trace': self.textbuffer.create_tag("trace", foreground="#18186f", invisible=self.enabledTypes["trace"] != True, invisible_set=True)
+        self.enabled_types = {'info': True, 'warning': True, 'error': True, 'trace': False}
+        self.type_tags = {
+            'info': self.textbuffer.create_tag("info",
+                                               foreground="#1a6f18",
+                                               invisible=not self.enabled_types["info"],
+                                               invisible_set=True),
+            'warning': self.textbuffer.create_tag("warning",
+                                                  foreground="#c8bf33",
+                                                  invisible=not self.enabled_types["warning"],
+                                                  invisible_set=True),
+            'error': self.textbuffer.create_tag("error",
+                                                foreground="#9f1313",
+                                                invisible=not self.enabled_types["error"],
+                                                invisible_set=True),
+            'trace': self.textbuffer.create_tag("trace",
+                                                foreground="#18186f",
+                                                invisible=not self.enabled_types["trace"],
+                                                invisible_set=True)
         }
 
         #todo: load all enabled types from gsettings
-        #self.enabledTypes = {'info': False, 'warning': False, 'error': False, 'trace': False }
+        #self.enabled_types = {'info': False, 'warning': False, 'error': False, 'trace': False }
         #for key in data:
-        #    self.enabledTypes[key] = True
-        self.getUpdates()
-
-        lookingGlassProxy.connect("LogUpdate", self.getUpdates)
-        lookingGlassProxy.addStatusChangeCallback(self.onStatusChange)
+        #    self.enabled_types[key] = True
+        self.get_updates()
+        self.proxy.connect("LogUpdate", self.get_updates)
+        self.proxy.add_status_change_callback(self.on_status_change)
 
     def append(self, category, time, message):
         entry = LogEntry(category, time, message)
         self.log.append(entry)
         return entry
 
-    def onButtonToggled(self, button, data):
+    def on_button_toggled(self, button, data):
         active = button.get_active()
-        self.enabledTypes[data] = active
-        self.typeTags[data].props.invisible = active != True
+        self.enabled_types[data] = active
+        self.type_tags[data].props.invisible = not active
         self.textbuffer.set_modified(True)
 
-    def onStatusChange(self, online):
-        textIter = self.textbuffer.get_end_iter()
+    def on_status_change(self, online):
+        text_iter = self.textbuffer.get_end_iter()
         if online:
-            entry = self.append("info", 0, "================ DBus connection established ===============")
+            entry = self.append("info",
+                                0,
+                                "================ DBus connection established ===============")
         else:
-            entry = self.append("warning", 0, "================ DBus connection lost ===============")
-        self.textbuffer.insert_with_tags(textIter, entry.formattedText, self.typeTags[entry.category])
-        self.getUpdates(True)
+            entry = self.append("warning",
+                                0,
+                                "================ DBus connection lost ===============")
+        self.textbuffer.insert_with_tags(text_iter,
+                                         entry.formatted_text,
+                                         self.type_tags[entry.category])
+        self.get_updates(True)
 
-    def getUpdates(self, reread = False):
-        success, data = lookingGlassProxy.GetErrorStack()
+    def get_updates(self, reread=False):
+        success, data = self.proxy.GetErrorStack()
         if success:
             try:
-                dataSize = len(data)
-                if dataSize > 0:
+                data_size = len(data)
+                if data_size > 0:
                     # If this is a completely new log, start reading at the beginning
-                    firstMessageTime = data[0]["timestamp"]
-                    if self.addedMessages > dataSize or self.firstMessageTime != firstMessageTime or reread:
-                        self.firstMessageTime = firstMessageTime
-                        self.addedMessages = 0
+                    first_message_time = data[0]["timestamp"]
+                    if (self.added_messages > data_size or
+                            self.first_message_time != first_message_time or
+                            reread):
+                        self.first_message_time = first_message_time
+                        self.added_messages = 0
 
                     if reread:
                         start, end = self.textbuffer.get_bounds()
                         self.textbuffer.delete(start, end)
 
-                    textIter = self.textbuffer.get_end_iter()
-                    for item in data[self.addedMessages:]:
-                        entry = self.append(item["category"], float(item["timestamp"])*0.001, item["message"])
-                        self.textbuffer.insert_with_tags(textIter, entry.formattedText, self.typeTags[entry.category])
-                        self.addedMessages += 1
+                    text_iter = self.textbuffer.get_end_iter()
+                    for item in data[self.added_messages:]:
+                        entry = self.append(item["category"],
+                                            float(item["timestamp"]) * 0.001,
+                                            item["message"])
+                        self.textbuffer.insert_with_tags(text_iter,
+                                                         entry.formatted_text,
+                                                         self.type_tags[entry.category])
+                        self.added_messages += 1
                     self.textview.scroll_to_mark(self.scroll_mark, 0, True, 1, 1)
-            except Exception as e:
-                print(e)
+            except Exception as exc:
+                print(exc)
 
 class ModulePage(pageutils.WindowAndActionBars):
     def __init__(self, parent):
-        self.view = LogView()
+        self.view = LogView(parent.lg_proxy)
         pageutils.WindowAndActionBars.__init__(self, self.view)
         self.parent = parent
 
-        self.addToggleButton("info", "dialog-information-symbolic", "Show/Hide Messages tagged as 'info'")
-        self.addToggleButton("warning", "dialog-warning-symbolic", "Show/Hide Messages tagged as 'warning'")
-        self.addToggleButton("error", "dialog-error-symbolic", "Show/Hide Messages tagged as 'error'")
-        self.addToggleButton("trace", "dialog-question-symbolic", "Show/Hide Messages tagged as 'trace'")
+        self.add_toggle_button("info",
+                               "dialog-information-symbolic",
+                               "Show/Hide Messages tagged as 'info'")
+        self.add_toggle_button("warning",
+                               "dialog-warning-symbolic",
+                               "Show/Hide Messages tagged as 'warning'")
+        self.add_toggle_button("error",
+                               "dialog-error-symbolic",
+                               "Show/Hide Messages tagged as 'error'")
+        self.add_toggle_button("trace",
+                               "dialog-question-symbolic",
+                               "Show/Hide Messages tagged as 'trace'")
 
-    def addToggleButton(self, logType, icon, tooltip):
+    def add_toggle_button(self, log_type, icon, tooltip):
         button = pageutils.ImageToggleButton(icon)
-        button.connect("toggled", self.view.onButtonToggled, logType)
-        button.set_active(self.view.enabledTypes[logType])
+        button.connect("toggled", self.view.on_button_toggled, log_type)
+        button.set_active(self.view.enabled_types[log_type])
         button.set_tooltip_text(tooltip)
-        self.addToLeftBar(button, 1)
+        self.add_to_left_bar(button, 1)
