147a2c7f3a05f50d81b2fc0e1cb7775e74533511
pyroxar <kacper-paczos@linux.pl>
2019-09-06 19:06:04 +0200

    I added "systemd-manager-pkexec" again.

--- files/usr/share/cinnamon/cinnamon-settings/cinnamon-settings.py
+++ files/usr/share/cinnamon/cinnamon-settings/cinnamon-settings.py
@@ -91,7 +91,8 @@ STANDALONE_MODULES = [
     [_("Package Management"),            "yumex-dnf",                           "cs-sources",         "admin",          _("update, install, repository, package, source, download")],
     [_("Users and Groups"),              "cinnamon-settings-users",             "cs-user-accounts",   "admin",          _("user, users, account, accounts, group, groups, password")],
     [_("Bluetooth"),                     "blueberry",                           "cs-bluetooth",       "hardware",       _("bluetooth, dongle, transfer, mobile")],
-    [_("Backup"),                        "pkexec timeshift-gtk",                "timeshift",          "admin",          _("backup, timeshift, files, copy, snapshots")]
+    [_("Backup"),                        "pkexec timeshift-gtk",                "timeshift",          "admin",          _("backup, timeshift, files, copy, snapshots")],
+    [_("Manage Services and Units"),     "systemd-manager-pkexec",              "cs-sources",         "admin",          _("systemd, units, services, systemctl, init")]
 ]
 
 TABS = {
