fde1e3bb4309ea516a27bfa47a58f1390db460fd
pyroxar <kacper-paczos@linux.pl>
2019-10-09 15:22:01 +0200

    Initial commit, nothing more

---
+++ files/usr/share/cinnamon/cinnamon-settings/modules/cs_nightlight.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from GSettingsWidgets import *
+
+class Module:
+    name = "night light"
+    comment = _("Cinnamon redshift settings")
+    category = "prefs"
+
+    def __init__(self, content_box):
+        WIP
