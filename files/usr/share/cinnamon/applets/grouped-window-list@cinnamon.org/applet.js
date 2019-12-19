5ef00cae75a9263a675ecc7cf1e83ee18ecd8de4
Michael Webster <miketwebster@gmail.com>
2019-12-19 14:46:26 -0500

    grouped-window-list: Establish 2 zones for dnd - non-favorited apps, or ungrouped additional favorited apps cannot be dragged amongst favorite positions (on the left), and favorites cannot be dragged amongst non-favorite or ungrouped items.

--- files/usr/share/cinnamon/applets/grouped-window-list@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/grouped-window-list@cinnamon.org/applet.js
@@ -706,6 +706,16 @@ class GroupedWindowListApplet extends Applet.Applet {
             }
         });
 
+        if (pos < this.pinnedFavorites._favorites.length && !source.groupState.isFavoriteApp) {
+            this.state.dragPlaceholderPos = pos;
+            return DND.DragMotionResult.NO_DROP;
+        }
+
+        if (pos > this.pinnedFavorites._favorites.length && source.groupState.isFavoriteApp) {
+            this.state.dragPlaceholderPos = pos;
+            return DND.DragMotionResult.NO_DROP;
+        }
+
         if (pos !== this.state.dragPlaceholderPos) {
             this.state.dragPlaceholderPos = pos;
 
@@ -771,6 +781,14 @@ class GroupedWindowListApplet extends Applet.Applet {
         let currentAppList = this.getCurrentAppList();
         let pos = this.state.dragPlaceholderPos;
         this.clearDragPlaceholder();
+
+        if (pos < this.pinnedFavorites._favorites.length && !source.groupState.isFavoriteApp) {
+            return false;
+        }
+
+        if (pos > this.pinnedFavorites._favorites.length && source.groupState.isFavoriteApp) {
+            return false;
+        }
 
         // For getting the fallback position, possibly dead code
         if (pos === -1) {
