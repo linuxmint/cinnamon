6a8ece3d8f209ded31ac9741be8f9587e46bdf05
JosephMcc <mccullarjoseph@gmail.com>
2020-01-11 21:42:13 -0800

    restart-osd: Add an additional style class
    This is just to allow themes a bit more flexibility in sizing the text

--- js/ui/main.js
+++ js/ui/main.js
@@ -1531,7 +1531,9 @@ function getTabList(workspaceOpt, screenOpt) {
 }
 
 function restartCinnamon() {
-    new ModalDialog.InfoOSD(_("Restarting Cinnamon...")).show();
+    let dialog = new ModalDialog.InfoOSD(_("Restarting Cinnamon..."));
+    dialog.actor.add_style_class_name('restart-osd');
+    dialog.show();
 
     global.reexec_self();
 }
