e9be9b3e7e65d29ecfc841a833ad7f4295f99c08
Michael Webster <miketwebster@gmail.com>
2020-02-06 12:13:17 -0500

    cs_display: Remove the ui scale setting from the second page.
    This is managed in monitor configurations now.

--- files/usr/share/cinnamon/cinnamon-settings/modules/cs_display.py
+++ files/usr/share/cinnamon/cinnamon-settings/modules/cs_display.py
@@ -36,10 +36,6 @@ class Module:
             self.sidePage.stack.add_titled(page, "settings", _("Settings"))
             settings = page.add_section(_("Settings"))
 
-            ui_scales = [[0, _("Auto")], [1, _("Normal")], [2, _("Double (Hi-DPI)")]]
-            combo = GSettingsComboBox(_("User interface scaling:"), "org.cinnamon.desktop.interface", "scaling-factor", ui_scales, valtype=int)
-            settings.add_row(combo)
-
             switch = GSettingsSwitch(_("Disable automatic screen rotation"), "org.cinnamon.settings-daemon.peripherals.touchscreen", "orientation-lock")
             switch.set_tooltip_text(_("Select this option to disable automatic screen rotation on hardware equipped with supported accelerometers."))
             settings.add_row(switch)
