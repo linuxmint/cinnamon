25ec5380eec09df336a4fe2609029c340f3b5e92
Michael Webster <miketwebster@gmail.com>
2021-02-25 23:17:57 -0500

    power applet: Adjust warning and critical levels to match the correctly colored icons in the set.
    The batter-level-10 icon has a warning-colored emblem, the 0 icon is
    error (critical).

--- files/usr/share/cinnamon/applets/power@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/power@cinnamon.org/applet.js
@@ -501,12 +501,12 @@ class CinnamonPowerApplet extends Applet.TextIconApplet {
         }
 
         if (device_type == UPDeviceType.BATTERY) {
-            if (percentage > 20) {
-                this._applet_icon.set_style_class_name('system-status-icon');
-            } else if (percentage > 5) {
+            if (percentage < 20) {
                 this._applet_icon.set_style_class_name('system-status-icon warning');
-            } else {
+            } else if (percentage < 10) {
                 this._applet_icon.set_style_class_name('system-status-icon error');
+            } else {
+                this._applet_icon.set_style_class_name('system-status-icon');
             }
         } else {
             this._applet_icon.set_style_class_name ('system-status-icon');
