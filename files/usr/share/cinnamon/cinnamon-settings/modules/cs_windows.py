a895babecf72db544c33e54b0d8a64d1df41dde1
JosephMcc <mccullarjoseph@gmail.com>
2021-03-12 19:52:38 -0800

    cs_windows: Fix the button layout when placing icons on the right
    These should mirror the layout of having them on the left
    
    Fixes: https://github.com/linuxmint/cinnamon/issues/9522

--- files/usr/share/cinnamon/cinnamon-settings/modules/cs_windows.py
+++ files/usr/share/cinnamon/cinnamon-settings/modules/cs_windows.py
@@ -36,7 +36,7 @@ class Module:
 
             button_options = []
             button_options.append([":minimize,maximize,close", _("Right")])
-            button_options.append(["close,minimize,maximize:", _("Left")])
+            button_options.append(["close,maximize,minimize:", _("Left")])
             button_options.append([":close", _("Gnome")])
             button_options.append(["close:minimize,maximize", _("Classic Mac")])
 
