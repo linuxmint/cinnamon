62ce419025098e4779e8276cefe8ca226747b9bb
Michael Webster <miketwebster@gmail.com>
2021-04-05 14:33:48 -0400

    Spice updater: Add harvester module.

--- files/usr/lib/python3/dist-packages/cinnamon/updates.py
+++ files/usr/lib/python3/dist-packages/cinnamon/updates.py
@@ -9,8 +9,8 @@ gi.require_version('Gtk', '3.0')
 
 gettext.install("cinnamon", "/usr/share/locale", names=["ngettext"])
 
-sys.path.append("/usr/share/cinnamon/cinnamon-settings/bin")
-from Spices import Spice_Harvester
+from . import _harvester
+from ._harvester import Update
 
 SPICE_TYPE_APPLET = "applet"
 SPICE_TYPE_DESKLET = "desklet"
@@ -18,26 +18,11 @@ SPICE_TYPE_THEME = "theme"
 SPICE_TYPE_EXTENSION = "extension"
 SPICE_TYPES = [SPICE_TYPE_APPLET, SPICE_TYPE_DESKLET, SPICE_TYPE_THEME, SPICE_TYPE_EXTENSION]
 
-class Update():
-
-    def __init__(self, uuid, spice_type):
-        self.uuid = uuid
-        self.spice_type = spice_type
-        self.new_version = "234"
-        self.old_version = "121"
-        self.size = 12124
-        self.name = "Some name"
-        self.description = "Some description"
-        self.commit_id = "123123asd123"
-        self.commit_msg = "blah blah"
-        self.link = "https://cinnamon-spices.linuxmint.com/applets/view/322"
-
 class UpdateManager():
-
     def __init__(self):
         self.harvesters = {}
         for spice_type in SPICE_TYPES:
-            self.harvesters[spice_type] = Spice_Harvester(spice_type, None)
+            self.harvesters[spice_type] = _harvester.Harvester(spice_type)
 
     def get_updates(self):
         updates = []
@@ -57,13 +42,9 @@ class UpdateManager():
         return updates
 
     def get_updates_of_type(self, spice_type):
-        updates = []
         harvester = self.harvesters[spice_type]
-        harvester.refresh_cache()
-        if harvester.are_updates_available():
-            for uuid in harvester.updates_available:
-                updates.append(Update(uuid, spice_type))
-        return updates
+        harvester.refresh()
+        return harvester.get_updates()
 
     def upgrade(self, update):
         self.upgrade_uuid(update.uuid, update.spice_type)
