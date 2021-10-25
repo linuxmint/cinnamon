91a66889d9aad7eab46d15d37478c8863c9902dd
Michael Webster <miketwebster@gmail.com>
2021-10-25 11:23:34 -0400

    Fix live window previews for expo.
    ClutterGroup must be ported to ClutterActor + ClutterFixedLayout.

--- js/misc/windowUtils.js
+++ js/misc/windowUtils.js
@@ -16,74 +16,6 @@ function createWindowClone(metaWindow, width, height, withTransients, withPositi
   if (!metaWindowActor) {
     return clones;
   }
-  // let texture = metaWindowActor.get_texture();
-  let [windowWidth, windowHeight] = metaWindowActor.get_size();
-  let [maxWidth, maxHeight] = [windowWidth, windowHeight];
-  let {x, y} = metaWindow.get_buffer_rect();
-  let [minX, minY] = [x, y];
-  let [maxX, maxY] = [minX + windowWidth, minY + windowHeight];
-  textures.push({t: metaWindowActor, x: x, y: y, w: windowWidth, h: windowHeight});
-  if (withTransients) {
-    metaWindow.foreach_transient(function(win) {
-      let metaWindowActor = win.get_compositor_private();
-      // texture = metaWindowActor.get_texture();
-      [windowWidth, windowHeight] = metaWindowActor.get_size();
-      let { x, y } = win.get_buffer_rect();
-      maxWidth = Math.max(maxWidth, windowWidth);
-      maxHeight = Math.max(maxHeight, windowHeight);
-      minX = Math.min(minX, x);
-      maxX = Math.max(maxX, x + windowWidth);
-      minY = Math.min(minY, y);
-      maxY = Math.max(maxY, y + windowHeight);
-      textures.push({t: metaWindowActor, x: x, y: y, w: windowWidth, h: windowHeight});
-    });
-  }
-  let scale = 1;
-  let scaleWidth = 1;
-  let scaleHeight = 1;
-  if (width) {
-    scaleWidth = Math.min(width/(maxX - minX), 1);
-  }
-  if (height) {
-    scaleHeight = Math.min(height/(maxY - minY), 1);
-  }
-  if (width || height) {
-    scale = Math.min(scaleWidth, scaleHeight);
-  }
-
-  for (let i = 0; i < textures.length; i++) {
-    let data = textures[i];
-    let [texture, texWidth, texHeight, x, y] = [data.t, data.w, data.h, data.x, data.y];
-    if (withPositions) {
-      x -= minX;
-      y -= minY;
-    }
-    let params = {};
-    params.source = texture;
-    if (scale != 1) {
-      params.width = Math.round(texWidth * scale);
-      params.height = Math.round(texHeight * scale);
-      x = Math.round(x * scale);
-      y = Math.round(y * scale);
-    }
-    let clone = {actor: new Clutter.Clone(params), x: x, y: y};
-    clones.push(clone);
-  }
-  return clones;
-}
-
-function createLiveWindowClone(metaWindow, width, height, withTransients, withPositions) {
-  let clones = [];
-  let textures = [];
-
-  if (!metaWindow) {
-    return clones;
-  }
-
-  let metaWindowActor = metaWindow.get_compositor_private();
-  if (!metaWindowActor) {
-    return clones;
-  }
   let texture = metaWindowActor.get_texture();
   let [windowWidth, windowHeight] = metaWindowActor.get_size();
   let [maxWidth, maxHeight] = [windowWidth, windowHeight];
@@ -141,7 +73,7 @@ function createLiveWindowClone(metaWindow, width, height, withTransients, withPo
     y = Math.round(y * scale);
 
     let clone = { actor: actor, x: x, y: y };
-
+    texture.invalidate();
     clones.push(clone);
   }
   return clones;
