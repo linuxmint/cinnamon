0c154ed489d3b9bb14c0bfc5f4e213e1b7cd147a
Michael Webster <miketwebster@gmail.com>
2021-11-29 12:36:55 -0500

    flashspot.js: Use class expression to fix subclassing of Lightbox.
    This broke highlighting of applets added to the panel.

--- js/ui/flashspot.js
+++ js/ui/flashspot.js
@@ -8,27 +8,26 @@ const Tweener = imports.ui.tweener;
 
 const FLASHSPOT_ANIMATION_TIME = 0.15; // seconds
 
-function Flashspot(area) {
-   this._init(area);
-}
-Flashspot.prototype = {
-   __proto__: Lightbox.Lightbox.prototype,
+var Flashspot = class Flashspot extends Lightbox.Lightbox {
+    constructor(area) {
+        super(
+            Main.uiGroup,
+            {
+                inhibitEvents: true,
+                width: area.width,
+                height: area.height
+            }
+        );
 
-   _init: function(area) {
-      Lightbox.Lightbox.prototype._init.call(this, Main.uiGroup, {
-         inhibitEvents: true,
-         width: area.width,
-         height: area.height
-      });
-      this.actor.style_class = 'flashspot';
-      this.actor.set_position(area.x, area.y);
-      if (area.time)
-        this.animation_time = area.time;
-      else
-        this.animation_time = FLASHSPOT_ANIMATION_TIME;
-   },
+        this.actor.style_class = 'flashspot';
+        this.actor.set_position(area.x, area.y);
+        if (area.time)
+            this.animation_time = area.time;
+        else
+            this.animation_time = FLASHSPOT_ANIMATION_TIME;
+   }
 
-   fire: function() {
+   fire() {
       this.actor.opacity = 0;
       Tweener.addTween(this.actor,
                       { opacity: 255,
@@ -37,9 +36,9 @@ Flashspot.prototype = {
                         onComplete: Lang.bind(this, this._onFireShowComplete)
                       });
       this.actor.show();
-   },
+   }
 
-   _onFireShowComplete: function() {
+   _onFireShowComplete () {
       Tweener.addTween(this.actor,
                       { opacity: 0,
                         time: this.animation_time,
