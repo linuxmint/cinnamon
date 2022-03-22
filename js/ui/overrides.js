2304d828b5ed986e8493a6b4596e21e9abe8458e
Michael Webster <miketwebster@gmail.com>
2022-03-22 10:11:02 -0400

    Remove gsettings overrides. They're in cjs now

--- js/ui/overrides.js
+++ js/ui/overrides.js
@@ -10,91 +10,11 @@ const Meta = imports.gi.Meta;
 
 function init() {
     overrideDumpStack();
-    overrideGio();
     overrideGObject();
     overrideMainloop();
     overrideJS();
     overrideClutter();
     overrideMeta();
-}
-
-function check_schema_and_init(obj, method, params) {
-    if (!params.schema) {
-        method.call(obj, params);
-        return;
-    }
-    let listSchemas = Gio.Settings.list_schemas();
-    if (listSchemas.indexOf(params.schema) != -1) {
-        method.call(obj, params);
-    } else {
-        method.call(obj, { schema_id: "org.cinnamon.invalid-schema" });
-        log("GSettings schema not found: " + params.schema);
-        throw new Error("GSettings schema not found: " + params.schema);
-    }
-}
-
-function key_exists (obj, key) {
-    return obj.list_keys().indexOf(key) != -1;
-}
-
-function check_key_and_get (obj, method, key) {
-    if (key_exists (obj, key)) {
-        return method.call(obj, key);
-    } else {
-        log("GSettings key not found. schema: " + obj.schema + ", key: " + key);
-        return null;
-    }
-}
-
-function check_key_and_set (obj, method, key, val) {
-    if (key_exists (obj, key)) {
-        return method.call(obj, key, val);
-    } else {
-        log("GSettings key not found. schema: " + obj.schema + ", key: " + key);
-        return false;
-    }
-}
-
-function overrideGio() {
-    Gio._real_init         = Gio.Settings.prototype._init;
-    Gio._real_get_value    = Gio.Settings.prototype.get_value;
-    Gio._real_set_value    = Gio.Settings.prototype.set_value;
-    Gio._real_get_boolean  = Gio.Settings.prototype.get_boolean;
-    Gio._real_set_boolean  = Gio.Settings.prototype.set_boolean;
-    Gio._real_get_int      = Gio.Settings.prototype.get_int;
-    Gio._real_set_int      = Gio.Settings.prototype.set_int;
-    Gio._real_get_uint     = Gio.Settings.prototype.get_uint;
-    Gio._real_set_uint     = Gio.Settings.prototype.set_uint;
-    Gio._real_get_double   = Gio.Settings.prototype.get_double;
-    Gio._real_set_double   = Gio.Settings.prototype.set_double;
-    Gio._real_get_string   = Gio.Settings.prototype.get_string;
-    Gio._real_set_string   = Gio.Settings.prototype.set_string;
-    Gio._real_get_strv     = Gio.Settings.prototype.get_strv;
-    Gio._real_set_strv     = Gio.Settings.prototype.set_strv;
-    Gio._real_get_enum     = Gio.Settings.prototype.get_enum;
-    Gio._real_set_enum     = Gio.Settings.prototype.set_enum;
-    Gio._real_get_flags    = Gio.Settings.prototype.get_flags;
-    Gio._real_set_flags    = Gio.Settings.prototype.set_flags;
-
-    Gio.Settings.prototype._init        = function(params)   { check_schema_and_init(this, Gio._real_init, params); }
-    Gio.Settings.prototype.get_value    = function(key)      { return check_key_and_get(this, Gio._real_get_value, key); }
-    Gio.Settings.prototype.set_value    = function(key, val) { return check_key_and_set(this, Gio._real_set_value, key, val); }
-    Gio.Settings.prototype.get_boolean  = function(key)      { return check_key_and_get(this, Gio._real_get_boolean, key); }
-    Gio.Settings.prototype.set_boolean  = function(key, val) { return check_key_and_set(this, Gio._real_set_boolean, key, val); }
-    Gio.Settings.prototype.get_int      = function(key)      { return check_key_and_get(this, Gio._real_get_int, key); }
-    Gio.Settings.prototype.set_int      = function(key, val) { return check_key_and_set(this, Gio._real_set_int, key, val); }
-    Gio.Settings.prototype.get_uint     = function(key)      { return check_key_and_get(this, Gio._real_get_uint, key); }
-    Gio.Settings.prototype.set_uint     = function(key, val) { return check_key_and_set(this, Gio._real_set_uint, key, val); }
-    Gio.Settings.prototype.get_double   = function(key)      { return check_key_and_get(this, Gio._real_get_double, key); }
-    Gio.Settings.prototype.set_double   = function(key, val) { return check_key_and_set(this, Gio._real_set_double, key, val); }
-    Gio.Settings.prototype.get_string   = function(key)      { return check_key_and_get(this, Gio._real_get_string, key); }
-    Gio.Settings.prototype.set_string   = function(key, val) { return check_key_and_set(this, Gio._real_set_string, key, val); }
-    Gio.Settings.prototype.get_strv     = function(key)      { return check_key_and_get(this, Gio._real_get_strv, key); }
-    Gio.Settings.prototype.set_strv     = function(key, val) { return check_key_and_set(this, Gio._real_set_strv, key, val); }
-    Gio.Settings.prototype.get_enum     = function(key)      { return check_key_and_get(this, Gio._real_get_enum, key); }
-    Gio.Settings.prototype.set_enum     = function(key, val) { return check_key_and_set(this, Gio._real_set_enum, key, val); }
-    Gio.Settings.prototype.get_flags    = function(key)      { return check_key_and_get(this, Gio._real_get_flags, key); }
-    Gio.Settings.prototype.set_flags    = function(key, val) { return check_key_and_set(this, Gio._real_set_flags, key, val); }
 }
 
 function overrideDumpStack() {
