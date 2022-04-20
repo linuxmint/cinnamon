f604f5ce02c74144e89910c3730a41a88ca646b6
Michael Webster <miketwebster@gmail.com>
2022-04-20 14:23:00 -0400

    Let muffin manage stacking of the background actor, desklet and desktop containers.

--- js/ui/main.js
+++ js/ui/main.js
@@ -393,10 +393,7 @@ function start() {
                         let height = global.stage.height;
                         [alloc.min_size, alloc.natural_size] = [height, height];
                     });
-    global.reparentActor(global.background_actor, uiGroup);
     global.background_actor.hide();
-    global.reparentActor(global.bottom_window_group, uiGroup);
-    uiGroup.add_actor(deskletContainer.actor);
     global.reparentActor(global.window_group, uiGroup);
     global.reparentActor(global.overlay_group, uiGroup);
 
