323b5aa3f3070168956c46cbb0ba140fe66215e8
Attila Greguss <floyd0122@gmail.com>
2022-11-04 20:03:24 +0000

    Use `XDG_STATE_HOME` for logfiles instead of `~/.cinnamon` (#11186)
    * Use `XDG_STATE_HOME` for logfiles instead of `~/.cinnamon`
    
    Although I've not seen any of these logfiles on my system.
    
    glass.log entry was removed because it doesn't exists anymore.
    
    * Add try/except around `get_user_state_dir` for backwards compatibility

--- python3/cinnamon/logger.py
+++ python3/cinnamon/logger.py
@@ -3,9 +3,14 @@
 import os
 import threading
 import queue
+from pathlib import Path
+from gi.repository import GLib
 
 # Share among multiple Harvesters
-logfile = '%s/.cinnamon/harvester.log' % os.path.expanduser("~")
+try:
+    logfile = os.path.join(GLib.get_user_state_dir(), 'cinnamon', 'harvester.log')
+except AttributeError:
+    logfile = '%s/.cinnamon/harvester.log' % os.path.expanduser("~")
 
 class ActivityLogger():
     def __init__(self):
@@ -17,6 +22,9 @@ class ActivityLogger():
         self.queue.put(entry)
 
     def write_to_file_thread(self):
+        directory = Path(logfile).parent
+        if not directory.exists():
+            os.makedirs(directory)
         while True:
             entry = self.queue.get()
             with open(logfile, "a") as f:
