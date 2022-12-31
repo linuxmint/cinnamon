8c7f868772f2f832cbb73b9ba2c863040a00a8ec
Daniella Mesquita <mesquitadaniella@proton.me>
2022-12-31 20:28:29 -0300

    add custom/panel/PanelContextMenu/systemmonitor (50%)

---
+++ js/ui/custom/panel/PanelContextMenu/systemmonitor/b.js
@@ -0,0 +1,2 @@
+this.addMenuItem(new AppLauncher(_("System Monitor"), "gnome-system-monitor", "system-monitor-app-symbolic"));
+// did this thanks to https://forums.linuxmint.com/viewtopic.php?p=2163518#p2163518 who had the same desire as me
