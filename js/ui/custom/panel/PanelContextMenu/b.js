7fa5ccf41c2041430ab7d2ef18e7d0c5bd1a5f2e
Daniella Mesquita <mesquitadaniella@proton.me>
2023-01-01 13:34:49 -0300

    add appending tips to ui/custom/panel/PanelContextMenu

--- js/ui/custom/panel/PanelContextMenu/b.js
+++ js/ui/custom/panel/PanelContextMenu/b.js
@@ -1,2 +1,4 @@
 //For example, uncomment the bellow line (do the same in the opposing a.js file):
 //imports.ui.custom.panel.PanelContextMenu.systemmonitor.b;
+
+//Declaring new custom menu items, will be appended after this line (the same should happen in the opposing b.js file)
