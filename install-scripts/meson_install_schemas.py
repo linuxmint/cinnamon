9a5a5b7d9cf14b4e44899c1918c2755255e798ea
Michael Webster <miketwebster@gmail.com>
2023-05-25 21:45:58 -0400

    Halt the build if meson_install_schemas.py fails.
    In Mint 21's meson version, there's no output if glib-compile-schemas
    fails. This is fixed in a later version, but the build should still
    fail regardless if that command does.

--- install-scripts/meson_install_schemas.py
+++ install-scripts/meson_install_schemas.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
 import os
+import sys
 import subprocess
 
 schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
 
 if not os.environ.get('DESTDIR'):
     print('Compiling gsettings schemas...')
-    subprocess.call(['glib-compile-schemas', schemadir])
+    out = subprocess.run(['glib-compile-schemas', "--strict", schemadir], capture_output=True)
+
+    if out.returncode != 0:
+        print("compile error: ", out.stderr)
+        sys.exit(1)
