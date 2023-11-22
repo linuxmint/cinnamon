f4884e42b00ae17b45d7564f6aecf2477768bbe9
ZornsLemma <sgf@lemma.co.uk>
2023-11-22 19:21:16 +0000

    Use menu for window list hover tooltip choice (#11912)
    The existing behaviours (window title or window thumbnail) are offered,
    as well as a new "nothing" option which disables any kind of tooltip.

--- files/usr/share/cinnamon/applets/window-list@cinnamon.org/applet.js
+++ files/usr/share/cinnamon/applets/window-list@cinnamon.org/applet.js
@@ -386,7 +386,7 @@ class AppMenuButton {
         if (this._tooltip)
             this._tooltip.destroy();
 
-        if (this._applet.usePreview)
+        if (this._applet.windowHover == "thumbnail")
             this._tooltip = new WindowPreview(this, this.metaWindow, this._applet.previewScale, this._applet.showLabel);
         else
             this._tooltip = new Tooltips.PanelItemTooltip(this, "", this._applet.orientation);
@@ -521,7 +521,7 @@ class AppMenuButton {
         if (title.length > MAX_TEXT_LENGTH)
             title = title.substr(0, MAX_TEXT_LENGTH);
 
-        if (this._tooltip  && this._tooltip.set_text)
+        if (this._tooltip && this._applet.windowHover != "nothing" && this._tooltip.set_text)
             this._tooltip.set_text(title);
 
         if (this.metaWindow.minimized) {
@@ -1046,7 +1046,7 @@ class CinnamonWindowListApplet extends Applet.Applet {
         this.settings.bind("button-width", "buttonWidth", this._refreshAllItems);
         this.settings.bind("buttons-use-entire-space", "buttonsUseEntireSpace", this._refreshAllItems);
         this.settings.bind("panel-show-label", "showLabelPanel", this._updateLabels);
-        this.settings.bind("window-preview", "usePreview", this._onPreviewChanged);
+        this.settings.bind("window-hover", "windowHover", this._onPreviewChanged);
         this.settings.bind("window-preview-show-label", "showLabel", this._onPreviewChanged);
         this.settings.bind("window-preview-scale", "previewScale", this._onPreviewChanged);
         this.settings.bind("last-window-order", "lastWindowOrder", null);
