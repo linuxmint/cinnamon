242065a3c5afacdc45499038f9adc4bce0600f14
JosephMcc <mccullarjoseph@gmail.com>
2024-10-20 14:43:17 -0700

    polkitAuthenticationDialog: Use the new StPasswordEntry

--- js/ui/polkitAuthenticationAgent.js
+++ js/ui/polkitAuthenticationAgent.js
@@ -105,14 +105,14 @@ var AuthenticationDialog = GObject.registerClass({
             vertical: true,
         });
 
-        this._passwordEntry = new St.Entry({
+        this._passwordEntry = new St.PasswordEntry({
             style_class: 'prompt-dialog-password-entry',
             text: "",
             can_focus: true,
             visible: false,
             x_align: Clutter.ActorAlign.CENTER,
         });
-        CinnamonEntry.addContextMenu(this._passwordEntry, { isPassword: true });
+        CinnamonEntry.addContextMenu(this._passwordEntry);
         this._passwordEntry.clutter_text.connect('activate', this._onEntryActivate.bind(this));
         this._passwordEntry.bind_property('reactive',
             this._passwordEntry.clutter_text, 'editable',
@@ -120,6 +120,12 @@ var AuthenticationDialog = GObject.registerClass({
         passwordBox.add_child(this._passwordEntry);
 
         let warningBox = new St.BoxLayout({ vertical: true });
+
+        let capsLockWarning = new CinnamonEntry.CapsLockWarning();
+        this._passwordEntry.bind_property('visible',
+            capsLockWarning, 'visible',
+            GObject.BindingFlags.SYNC_CREATE);
+        warningBox.add_child(capsLockWarning);
 
         this._errorMessageLabel = new St.Label({
             style_class: 'prompt-dialog-error-label',
@@ -292,10 +298,7 @@ var AuthenticationDialog = GObject.registerClass({
         else
             this._passwordEntry.hint_text = request;
 
-        if (echoOn)
-            this._passwordEntry.clutter_text.set_password_char('');
-        else
-            this._passwordEntry.clutter_text.set_password_char('\u25cf'); // ● U+25CF BLACK CIRCLE
+        this._passwordEntry.password_visible = echoOn;
 
         this._passwordEntry.show();
         this._passwordEntry.set_text('');
