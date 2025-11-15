4aa8836ce797d947d37cbdb7b79ee9c2b16d5053
Kevin Langman <klangman@gmail.com>
2025-11-15 11:24:16 -0500

    Call on_applet_reloaded() for all instances of an Applet (#13103)
    When reloading an Applet that has more than one instance, the on_applet_reloaded() function is only called on the first Applet in the definitions array that matches the UUID. This fix allows the loop to continue through all Applet definitions and call on_applet_reloaded() for all Applet instances matching the UUID.

--- js/ui/appletManager.js
+++ js/ui/appletManager.js
@@ -122,7 +122,6 @@ function prepareExtensionReload(extension) {
             if (!applet) continue;
             global.log(`Reloading applet: ${extension.uuid}/${applet_id}`);
             applet.on_applet_reloaded();
-            return;
         }
     }
 }
