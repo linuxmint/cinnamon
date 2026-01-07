3a4dc837abcc744a1ea722cb0599a931f4adcddc
rgfegegeegege <eiudcedijdecijdce@protonmail.com>
2026-01-07 16:30:53 +0100

    Update utils.py

--- files/usr/share/cinnamon/applets/grouped-window-list@cinnamon.org/utils.py
+++ files/usr/share/cinnamon/applets/grouped-window-list@cinnamon.org/utils.py
@@ -24,93 +24,84 @@ def spawn(command):
 
 # Utility script that creates GDesktop files for Wine and other window backed applications.
 def handle_cli():
-    if CLI[1] == 'get_process':
-        if len(CLI) < 3 or not CLI[2].isdigit():
-            print("Invalid PID provided")
-            sys.exit(1)
+    if len(CLI) < 2 or CLI[1] != 'get_process':
+        return
 
-        pid = int(CLI[2])
-        process = spawn(f"cat /proc/{pid}/cmdline")
+    if len(CLI) < 3 or not CLI[2].isdigit():
+        print("Invalid PID provided")
+        sys.exit(1)
 
-        if '.exe' in process:
-            if 'Z:' in process:
-                process = process.split('Z:')[1]
+    pid = int(CLI[2])
+    process = spawn(f"cat /proc/{pid}/cmdline")
 
-            process = process.replace('\\', '/')
-            process = process.split('.exe')[0] + '.exe'
-            process = 'wine '+process.replace(' ', r'\ ')
+    if '.exe' in process:
+        if 'Z:' in process:
+            process = process.split('Z:')[1]
 
-        process = json.dumps(process)
-        if '\\u0000' in process:
-            process = process.replace('\\u0000', ' ')
-        process = json.loads(process)
+        process = process.replace('\\', '/')
+        process = process.split('.exe')[0] + '.exe'
+        process = 'wine ' + process.replace(' ', r'\ ')
 
-        if '.exe' not in process:
-            process = process[:-1]
+    # Remove trailing null byte from /proc/.../cmdline if not a Wine app
+    if not process.endswith('.exe'):
+        process = process[:-1]
 
-        if process == 'python mainwindow.py':
-            process = 'playonlinux'
+    if process == 'python mainwindow.py':
+        process = 'playonlinux'
+
+    try:
+        proc_array = process.split('/')
+        process_name = proc_array[-1].title()
+
+        # Since this is a window backed app, make sure it has an icon association.
+        icons_dir = os.path.join(os.getenv('HOME'), '.local/share/icons/hicolor/48x48/apps/')
+
+        if '\\ ' in process_name:
+            process_name = process_name.replace('\\ ', ' ')
+        if '.Exe' in process_name:
+            process_name = process_name.replace('.Exe', '')
+
+        icon_file = process_name + '.png'
+        if ' ' in icon_file:
+            icon_file = icon_file.replace(' ', '')
+
+        icon = os.path.join(icons_dir, icon_file)
 
         try:
-            proc_array = process.split('/')
-            pa_len = len(proc_array)
-            process_name = proc_array[pa_len - 1].title()
-
-            # Since this is a window backed app, make sure it has an icon association.
-
-            icons_dir = '{}/.local/share/icons/hicolor/48x48/apps/'.format(os.getenv('HOME'))
-
-            if '\\ ' in process_name:
-                process_name = process_name.replace('\\ ', ' ')
-
-            if '.Exe' in process_name:
-                process_name = process_name.replace('.Exe', '')
-
-            icon_file = process_name+'.png'
-
-            if ' ' in icon_file:
-                icon_file = icon_file.replace(' ', '')
-
-            icon = icons_dir+icon_file
-
             try:
-                try:
-                    spawn('gnome-exe-thumbnailer {} {}'.format(process.split('wine ')[1], icon))
-                except IndexError:
-                    spawn('gnome-exe-thumbnailer {} {}'.format(process, icon))
-            except subprocess.CalledProcessError:
-                icon = None
+                spawn('gnome-exe-thumbnailer {} {}'.format(process.split('wine ')[1], icon))
+            except IndexError:
+                spawn('gnome-exe-thumbnailer {} {}'.format(process, icon))
+        except subprocess.CalledProcessError:
+            icon = None
 
-            g_menu = '[Desktop Entry]\n' \
-                     'Type=Application\n' \
-                     'Encoding=UTF-8\n' \
-                     'Name={}\n' \
-                     'Comment={}\n' \
-                     'Exec={}\n' \
-                     'Terminal=false\n' \
-                     'StartupNotify=true\n'.format(process_name, process_name, process)
+        g_menu = '[Desktop Entry]\n' \
+                 'Type=Application\n' \
+                 'Encoding=UTF-8\n' \
+                 'Name={}\n' \
+                 'Comment={}\n' \
+                 'Exec={}\n' \
+                 'Terminal=false\n' \
+                 'StartupNotify=true\n'.format(process_name, process_name, process)
 
-            if icon:
-                g_menu += 'Icon={}\n'.format(icon)
+        if icon:
+            g_menu += 'Icon={}\n'.format(icon)
 
+        if '.exe' in process:
+            g_menu += 'GenericName=Wine application\n' \
+                      'Categories=Wine;\n' \
+                      'MimeType=application/x-ms-dos-executable;application/x-msi;application/x-ms-shortcut;\n'
 
-            if '.exe' in process:
-                g_menu += 'GenericName=Wine application\n' \
-                          'Categories=Wine;\n' \
-                          'MimeType=application/x-ms-dos-executable;' \
-                          'application/x-msi;application/x-ms-shortcut; \n' \
+        desktop_file = '{}.cinnamon-generated.desktop'.format(process_name)
+        desktop_path = os.path.join(os.getenv('HOME'), '.local/share/applications', desktop_file)
 
-            desktop_file = '{}.cinnamon-generated.desktop'.format(process_name)
-            desktop_path = '{}/.local/share/applications/{}'.format(os.getenv('HOME'), desktop_file)
+        with open(desktop_path, 'w', encoding='utf-8') as desktop:
+            print(g_menu)
+            desktop.write(g_menu)
+            spawn('chmod +x {}'.format(desktop_path))
+            print(desktop_file)
 
-            with open(desktop_path, 'w', encoding='utf-8') as desktop:
-                print(g_menu)
-                desktop.write(g_menu)
-                spawn('chmod +x {}'.format(desktop_path))
-                print(desktop_file)
-
-        except KeyError as err:
-            print(err)
-            return
+    except (KeyError, IndexError) as err:
+        print(err)
 
 handle_cli()
