63caebec815f09464fd174f99f6265af703d85da
Sunderland93 <samoilov.lex@gmail.com>
2026-02-20 17:01:37 +0400

    fix indents

--- js/ui/checkBox.js
+++ js/ui/checkBox.js
@@ -4,38 +4,38 @@ const Pango = imports.gi.Pango;
 const St = imports.gi.St;
 
 var CheckBox = GObject.registerClass(
-    class CheckBox extends St.Button {
-        _init(label) {
-            let container = new St.BoxLayout();
-            super._init({
-                style_class: 'check-box',
-                important: true,
-                child: container,
-                button_mask: St.ButtonMask.ONE,
-                toggle_mode: true,
-                can_focus: true,
-                x_fill: true,
-                y_fill: true,
-            });
+class CheckBox extends St.Button {
+    _init(label) {
+        let container = new St.BoxLayout();
+        super._init({
+            style_class: 'check-box',
+            important: true,
+            child: container,
+            button_mask: St.ButtonMask.ONE,
+            toggle_mode: true,
+            can_focus: true,
+            x_fill: true,
+            y_fill: true,
+        });
 
-            this._box = new St.Bin();
-            this._box.set_y_align(Clutter.ActorAlign.START);
-            container.add_actor(this._box);
+        this._box = new St.Bin();
+        this._box.set_y_align(Clutter.ActorAlign.START);
+        container.add_actor(this._box);
 
-            this._label = new St.Label({ y_align: Clutter.ActorAlign.CENTER });
-            this._label.clutter_text.set_line_wrap(true);
-            this._label.clutter_text.set_ellipsize(Pango.EllipsizeMode.NONE);
-            container.add_actor(this._label);
+        this._label = new St.Label({ y_align: Clutter.ActorAlign.CENTER });
+        this._label.clutter_text.set_line_wrap(true);
+        this._label.clutter_text.set_ellipsize(Pango.EllipsizeMode.NONE);
+        container.add_actor(this._label);
 
-            if (label)
-                this.setLabel(label);
-        }
+        if (label)
+            this.setLabel(label);
+    }
 
-        setLabel(label) {
-            this._label.set_text(label);
-        }
+    setLabel(label) {
+        this._label.set_text(label);
+    }
 
-        getLabelActor() {
-            return this._label;
-        }
-    });
+    getLabelActor() {
+        return this._label;
+    }
+});
