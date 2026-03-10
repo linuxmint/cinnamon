af4bfb2b96d44a48d68391b58c681cc7a8d53f3a
Hans-Fritz Pommes <valarmmail@gmx.de>
2026-03-10 15:52:48 +0100

    Update tool.py
    improved error handling

--- files/usr/share/cinnamon/applets/printers@cinnamon.org/tool.py
+++ files/usr/share/cinnamon/applets/printers@cinnamon.org/tool.py
@@ -5,15 +5,14 @@
 import subprocess
 import sys
 
-if len(sys.argv)>1 and not len(sys.argv)>2:
-    match sys.argv[1]:
-        case "lpstat-a":
-            lpstat_a = subprocess.run(['/usr/bin/lpstat', '-a'], capture_output=True)
-            if len(lpstat_a.stderr)==0:
-                print(lpstat_a.stdout.decode("utf-8"))
-            else:
-                print("ERRORS:\n"+lpstat_a.stderr.decode("utf-8"))
-        case _:
-            print("Wrong arguments")
+if len(sys.argv)>1:
+    try:
+        process = subprocess.run(sys.argv[1:], capture_output=True)
+        if len(process.stderr)==0:
+            print("OK:\n"+process.stdout.decode("utf-8"), end="")
+        else:
+            print("ERRORS:\n"+process.stderr.decode("utf-8"), end="")
+    except Exception as error:
+        print("ERRORS:\n"+str(error))
 else:
     print("Wrong arguments")
