950b61ac4f4c9b8da5b54d37bdcca7c7d306da78
Eric Wing <ew -dot- mailing |-at-| gmail.com>
2026-05-29 12:27:42 -0700

    Applet for scaleapp
    Now that scaleapp has been implemented, for consistency, created a new applet for scaleapp, based off the scale applet. The code is virtually identical except for the name changes, the icon, and one parameter to toggle() which activates scaleapp instead of scale.
    
    A distinct icon is needed to tell it appart from the scale icon since both may exist in the tray at the same time. The new icon is a modification of the scale icon, where there are 4 larger squares instead of 9 smaller squares. The thinking behind this is the number of application windows is less than the number of total windows, so the view will have fewer windows and be bigger.

---
+++ files/usr/share/cinnamon/applets/scaleapp@cinnamon.org/applet.js
@@ -0,0 +1,46 @@
+const Applet = imports.ui.applet;
+const Lang = imports.lang;
+const Main = imports.ui.main;
+const Settings = imports.ui.settings;
+
+class CinnamonScaleApplet extends Applet.IconApplet {
+    constructor(metadata, orientation, panel_height, instance_id) {
+        super(orientation, panel_height, instance_id);
+
+        try {
+            this.set_applet_icon_symbolic_name("cinnamon-scaleapp");
+            this.set_applet_tooltip(_("Scale App"));
+            this._hover_activates = false;
+
+            this.settings = new Settings.AppletSettings(this, metadata.uuid, this.instance_id);
+
+            this.settings.bind("activate-on-hover", "_hover_activates");
+
+            this.actor.connect('enter-event', Lang.bind(this, this._onEntered));
+        }
+        catch (e) {
+            global.logError(e);
+        }
+    }
+
+    on_applet_clicked(event) {
+        if (this._hover_activates)
+            return;
+        this.doAction();
+    }
+
+    _onEntered(event) {
+        if (!this._hover_activates || global.settings.get_boolean("panel-edit-mode"))
+            return;
+        this.doAction();
+    }
+
+    doAction() {
+        if (!Main.overview.animationInProgress)
+            Main.overview.toggle("scaleapp");
+    }
+}
+
+function main(metadata, orientation, panel_height, instance_id) {
+    return new CinnamonScaleApplet(metadata, orientation, panel_height, instance_id);
+}
