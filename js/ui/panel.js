384e2555ae8a59a1ed0126879cc3e09126543cdf
canklot <33100414+canklot@users.noreply.github.com>
2026-05-31 13:46:06 +0300

    Call _fullPanelLoad on monitor change to fix issue #12641
    If a secondary monitor is connected after cinnamon starts, the panels on the secondary monitor is not displayed. Calling the fullPanelLoad inside onMonitorsChanged fixes the issue.
    
    This problem is mentioned on issue #12641 and #12467

--- js/ui/panel.js
+++ js/ui/panel.js
@@ -942,6 +942,7 @@ var PanelManager = GObject.registerClass({
     }
 
     _onMonitorsChanged() {
+        this._fullPanelLoad();
         const oldCount = this.monitorCount;
         this.monitorCount = global.display.get_n_monitors();
 
