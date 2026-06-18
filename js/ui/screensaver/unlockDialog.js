63120cc16d4aff48e4451be89565eb69a6883ae5
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2026-06-18 18:30:59 +0200

    screensaver: Prevent user from typing while password is checked
    Setting reactive to False only prevents mouse interaction.

--- js/ui/screensaver/unlockDialog.js
+++ js/ui/screensaver/unlockDialog.js
@@ -275,10 +275,12 @@ class UnlockDialog extends St.BoxLayout {
             this._infoLabel.text = '';
 
             this._passwordEntry.reactive = false;
+            this._passwordEntry.clutter_text.editable = false;
             this._passwordEntry.hint_text = _("Checking...");
             this._passwordEntry.start_busy();
         } else {
             this._passwordEntry.reactive = true;
+            this._passwordEntry.clutter_text.editable = true;
             this._passwordEntry.end_busy();
         }
     }
